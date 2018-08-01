bbdfe0a8f35c2486546219c26d731b6adcf903ed
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-08-01 20:38:59 +0300

    Add support for the Batch script in case FAKE is missing.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,18 @@
 @echo off
 
-REM The script assumes that FAKE is in PATH.
+set toolpath=".\DotNetTools"
 
-fake run .\build.fsx %*
+where /Q fake
+
+if %ERRORLEVEL% neq 0 (
+
+    if not exist %toolpath%\fake (
+        echo FAKE does not exist. Installing...
+        if exist %toolpath% (
+            rmdir /S /Q %toolpath%
+        )
+        dotnet tool install fake-cli --tool-path %toolpath%
+    )
+
+    %toolpath%\fake run .\build.fsx %*
+) else fake run .\build.fsx %*

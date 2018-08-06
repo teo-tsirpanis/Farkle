6044cb5cf76098652b20cbf63b640783a9b896ba
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-08-06 22:55:34 +0300

    Add safe arrays and maps. Remove the problem matcher. It is not needed anyway.

--- src/Farkle/Utilities/IndexedCollections.fs
+++ src/Farkle/Utilities/IndexedCollections.fs
@@ -31,6 +31,9 @@ module Indexed =
 
     open Operators.Checked
 
+    /// Creates an `Indexed` object, with the ability to explicitly specify its type.
+    let create<'a> i: Indexed<'a> = Indexed i
+
     /// Converts an `Indexed` value to an actual object lased on the index in a specified list.
     let getfromList (i: Indexed<'a>) (list: #IReadOnlyList<'a>) =
         let i = i |> (fun (Indexed i) -> i) |> int
@@ -62,3 +65,63 @@ module IndexableWrapper =
     /// Sorts `Indexable` items based on their index and removes their wrapper.
     /// Duplicate indices do not raise an error.
     let collect x = x |> Seq.sortBy Indexable.index |> Seq.map item |> ImmutableArray.CreateRange
+
+/// An immutable array that exhibits good random-access performance and safe index access.
+/// It intentionally lacks methods such as `map` and `filter`. This type should be at the final stage of data manipulation.
+/// It is advised to work with sequences before, just until the end.
+/// Safe random access is faciliated through `Indexed` objects.
+type RandomAccessList<'a> = private RandomAccessList of ImmutableArray<'a>
+    with
+        /// O(n) Creates a random-access list. Data will be copied to this new list.
+        static member Create x = x |> ImmutableArray.CreateRange |> RandomAccessList
+        member private x.Value = x |> (fun (RandomAccessList x) -> x)
+        /// O(1) Gets the length of the list.
+        member x.Count = x.Value.Length
+        /// Gets the item at the given position the `Indexed` object points to.
+        /// Because it does not accept an arbitrary integer, it is less likely to accidentially fail.
+        member x.Item
+            with get (i: Indexed<'a>) = x.Value.[int i.Value]
+
+        /// Returns an `Indexed` object that points to the position at the given index, or fails if it is out of bounds.
+        member x.Indexed
+            with get (i: uint32) =
+                match i with
+                | i when i < uint32 x.Count -> i |> Indexed.create<'a> |> Ok
+                | i -> Error i
+        interface IEnumerable with
+            /// [omit]
+            member x.GetEnumerator() = (x.Value :> IEnumerable).GetEnumerator()
+        interface IEnumerable<'a> with
+            /// [omit]
+            member x.GetEnumerator() = (x.Value :> seq<_>).GetEnumerator()
+        interface IReadOnlyCollection<'a> with
+            /// [omit]
+            member x.Count = x.Count
+
+type [<Struct>] Keyed<'TKey,'TCorrespondingValue when 'TKey: comparison> = Keyed of 'TKey
+    with
+        member x.Key = x |> (fun (Keyed x) -> x)
+
+module Keyed =
+    let create<'TKey,'TCorrespondingValue when 'TKey: comparison> k: Keyed<'TKey,'TCorrespondingValue> = Keyed k
+
+type RandomAccessMap<'TKey,'TValue when 'TKey: comparison> = private RandomAccessMap of Map<'TKey,'TValue>
+    with
+        static member Create x = x |> Map.ofSeq |> RandomAccessMap
+        member private x.Value = x |> (fun (RandomAccessMap x) -> x)
+        member x.ContainsKey k = x.Value.ContainsKey k
+        member x.Count = x.Value.Count
+        member x.Item
+            with get (k: Keyed<'TKey, 'TValue>) = x.Value.[k.Key]
+
+        member x.Keyed
+            with get k =
+                match k with
+                | k when x.ContainsKey k -> k |> Keyed.create<'TKey,'TValue> |> Ok
+                | k -> Error k
+        interface IEnumerable with
+            member x.GetEnumerator() = (x.Value :> IEnumerable).GetEnumerator()
+        interface IEnumerable<KeyValuePair<'TKey,'TValue>> with
+            member x.GetEnumerator() = (x.Value :> seq<_>).GetEnumerator()
+        interface IReadOnlyCollection<KeyValuePair<'TKey,'TValue>> with
+            member x.Count = x.Count

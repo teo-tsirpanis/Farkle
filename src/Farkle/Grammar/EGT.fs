a2f009a9e0ae79b747036783fef3af1b72975fc9
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-10-05 00:12:13 +0300

    Add API to read EGT files from Base64 strings.

--- src/Farkle/Grammar/EGT.fs
+++ src/Farkle/Grammar/EGT.fs
@@ -5,20 +5,26 @@
 
 namespace Farkle.Grammar.GOLDParser
 
-open Farkle
+open System
 open System.IO
 
-/// Functions to convert EGT files to make a grammar.
+/// Functions to read EGT files to make a grammar.
 module EGT =
 
     /// Reads a stream that represents an EGT file and returns a `Grammar`.
-    /// The stream can be disposed when it ends.
     [<CompiledName("CreateFromStream")>]
     let ofStream stream =
         use r = new BinaryReader(stream)
         GrammarReader.read r
 
-    /// Reads an EGT file and returns a `Grammar`.
+    /// Reads a Base64-encoded string of the EGT file and returns a grammar.
+    [<CompiledName("CreateFromBase64String")>]
+    let ofBase64String x =
+        let x = Convert.FromBase64String x
+        use s = new MemoryStream(x)
+        ofStream s
+
+    /// Reads an EGT file from the file system and returns a `Grammar`.
     [<CompiledName("CreateFromFile")>]
     let ofFile path =
         use stream = File.OpenRead path
--- tests/Farkle.Benchmarks/SerializationBenchmark.fs
+++ tests/Farkle.Benchmarks/SerializationBenchmark.fs
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 Theodore Tsirpanis
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -19,50 +19,12 @@ type SerializationBenchmark() =
 
     let mutable base64EGT = ""
 
-    let mutable serialized = ""
-
-    let serializeIt (x: GOLDGrammar) =
-        let f = BinaryFormatter()
-        use memStream = new MemoryStream()
-        f.Serialize(memStream, x)
-        memStream.ToArray() |> Convert.ToBase64String
-
-    let deserializeIt x =
-        let memStream = new MemoryStream(Convert.FromBase64String x)
-        let f = BinaryFormatter()
-        f.Deserialize memStream :?> GOLDGrammar
-
     [<GlobalSetup>]
     member __.Setup() =
         let bytes = File.ReadAllBytes "inception.egt"
         base64EGT <- Convert.ToBase64String bytes
         logger.WriteLineInfo <| sprintf "EGT as Base-64: %d characters" base64EGT.Length
-        use stream = new MemoryStream(bytes)
-        serialized <- stream |> EGT.ofStream |> returnOrFail |> serializeIt
-        logger.WriteLineInfo <| sprintf "Serialized grammar as Base64: %d characters" serialized.Length
 
     [<Benchmark>]
-    /// The most straightforward option, encoding the EGT file in Base64.
-    /// ## Pros:
-    /// * Works outside .NET
-    /// * Version-tolerant
-    /// * Faster
-    /// * Space-efficient
-    /// ## Cons:
-    /// * Uses more memory and causes more GC collections (not actually a problem; it is supposed to be called only once)
     member __.Base64EGT() =
-        use stream = new MemoryStream(Convert.FromBase64String base64EGT)
-        stream |> EGT.ofStream |> returnOrFail
-
-    [<Benchmark>]
-    /// Another option, directly serializing the GOLDGrammar object using the binary serializer, with a crude version validator.
-    /// ## Pros:
-    /// * Uses less memory and causes less GC collections
-    /// ## Cons
-    /// * Works only inside .NET
-    /// * Not version-tolerant (it isn't supposed to be)
-    /// * Slower
-    /// * Cannot be optimized because it is a .NET internal process
-    /// * Less space-efficient
-    member __.Serialized() = serialized |> deserializeIt
-    
+        base64EGT |> EGT.ofBase64String |> returnOrFail

1b0fba06113af9ed94781153bfe780b6d0bd1086
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-11-27 19:04:18 +0200

    Add and document the character stream API.
    I don't like it; I might change it to an OOP-based one, since it has to be made mutable for efficiency..

--- src/Farkle/Utilities/CharStream.fs
+++ src/Farkle/Utilities/CharStream.fs
@@ -5,50 +5,96 @@
 
 namespace Farkle.Collections
 
+open Farkle
 open System
 
-type CharStreamIndex = private CharStreamIndex of uint64
-with
-    member x.Index = match x with CharStreamIndex x -> x
-
-type Position = {
-    Line: uint64
-    Column: uint64
-    Index: CharStreamIndex
-}
-
+/// An continuous range of characters that is
+/// stored by its starting index and length.
 type CharSpan = private CharSpan of start: uint64 * length: int
 
+/// A representation of a `CharStream` that stores
+/// the characters in one continuous area of memory.
+/// It is not recommended for large files.
 type private StaticBlock = {
     Stream: ReadOnlyMemory<char>
-    mutable Position: Position
+    Position: Position
 }
 
+/// A data structure that supports efficient and copy-free access to a read-only sequence of characters.
 type CharStream = private StaticBlock of StaticBlock
 with
+    /// The stream's current position.
     member x.Position = match x with StaticBlock sb -> sb.Position
+    /// The stream's character at its current position.
+    member x.FirstCharacter = match x with StaticBlock sb -> sb.Stream.Span.[int sb.Position.Index]
 
-type CharStreamViewer = private CharStreamViewer of stream: CharStream * idx: CharStreamIndex
+/// A type that gives a `CharStream` an F# `list`-like interface.
+type CharStreamView = internal CharStreamView of stream: CharStream * idx: uint64
 with
-    member x.Index = match x with | CharStreamViewer (_, idx) -> idx
+    /// The character index the view is in, starting from the beginning of the stream.
+    member x.Index = match x with | CharStreamView (_, idx) -> idx
 
+/// A .NET delegate that is the interface between the `CharStream` API and the post-processor.
+/// It accepts a generic type (a `Symbol` usually), the `Position` of the symbol, and a
+/// `ReadOnlySpan` of characters that are going to be converted into an object.
+/// This type is not an F# native function type, because of limitations while handling `ReadOnlySpan`s.
+type CharStreamCallback<'symbol> = delegate of 'symbol * Position * ReadOnlySpan<char> -> obj
+
+/// Functions to create and work with `CharStream`s.
 module CharStream =
 
+    open LanguagePrimitives
+    open Operators.Checked
+
+    /// Creates a `CharStreamView` from a `CharStream`.
     let view cs =
         match cs with
-        | StaticBlock sb -> CharStreamViewer (cs, cs.Position.Index)
-    let (|CSCons|CSNil|) (CharStreamViewer (cs, CharStreamIndex idx)) =
+        | StaticBlock sb -> CharStreamView (cs, sb.Position.Index)
+
+    /// An active pattern to access a `CharStreamView` as if it was an F# `list`.
+    let (|CSCons|CSNil|) (CharStreamView (cs, idx)) =
         match cs with
-        | StaticBlock sb when idx < uint64 sb.Stream.Length -> CSCons(sb.Stream.Span.[int idx], CharStreamViewer(cs, CharStreamIndex <| idx + 1UL))
+        | StaticBlock sb when idx < uint64 sb.Stream.Length ->
+            CSCons(sb.Stream.Span.[int idx], CharStreamView(cs, idx + GenericOne))
         | StaticBlock _ -> CSNil
 
-    let pinSpan cs (CharStreamIndex idxTo) =
+    /// Creates a `CharSpan` that contains the next `idxTo` characters of a `CharStream` from its position.
+    /// On the dynamic block character stream, it ensures that the characters in the span stay in memory.
+    /// Returns `None` if it is out of range, or the characters are not available.
+    let tryPinSpan cs idxTo =
         match cs with
-        | StaticBlock {Stream = b; Position = {Index = CharStreamIndex idxFrom}} when
-            idxFrom < idxTo && idxTo < uint64 b.Length && idxTo - idxFrom <= uint64 Int32.MaxValue ->
+        | StaticBlock {Stream = b; Position = {Index = idxFrom}}
+            when idxFrom < idxTo && idxTo < uint64 b.Length && idxTo - idxFrom < uint64 Int32.MaxValue ->
             Some <| CharSpan (idxFrom, int <| idxTo - idxFrom)
         | StaticBlock _ -> None
+        |> fun csp -> csp, cs
 
-    let appendSpan (CharSpan (start, length)) l = CharSpan (start, length + l)
+    /// Creates a new `CharSpan` from two continuous spans, i.e. the first one ends when the second one starts.
+    /// Returns `None` if they were not continuous.
+    let tryAppendSpans (CharSpan (start1, length1)) (CharSpan (start2, length2)) =
+        if start1 + uint64 length1 = start2 then
+            Some <| CharSpan (start1, length1 + length2)
+        else
+            None
 
-    let consume (CharSpan)
+    /// Advances the `CharStream`'s position by as many characters the given `CharSpan` indicates.
+    /// These characters will not be shown again on new `CharStreamView`s.
+    /// Calling this function does not affect the pinned `CharSpan`s.
+    let consume cs (CharSpan (start, length)) =
+        match cs with
+        | StaticBlock sb when sb.Position.Index = start ->
+            let mutable pos = sb.Position
+            let span = sb.Stream.Span.Slice(int start, length)
+            for i = 0 to span.Length - 1
+                do pos <- Position.advance span.[i] pos
+            {sb with Position = pos} |> StaticBlock
+        | StaticBlock _ -> cs
+
+    /// Creates an arbitrary object out of the characters at the given `CharSpan`.
+    /// After that call, these characters might be freed from memory, so this function should not be used twice.
+    let tryUnpinSpanAndGenerate symbol (fPostProcess: CharStreamCallback<'symbol>) cs (CharSpan (start, length)) =
+        match cs with
+        | StaticBlock sb when uint64 sb.Stream.Length > start + uint64 length ->
+            let span = sb.Stream.Span.Slice(int start, length)
+            fPostProcess.Invoke(symbol, sb.Position, span) |> Some, cs
+        | StaticBlock _ -> None, cs
--- src/Farkle/Utilities/Position.fs
+++ src/Farkle/Utilities/Position.fs
@@ -5,25 +5,16 @@
 
 namespace Farkle
 
-/// The zero-based index of a character in a stream.
-type CharacterIndex = private CharacterIndex of uint64
-with
-    /// The index's value.
-    member x.Index = match x with CharacterIndex x -> x
-
 /// A point in 2D space with integer coordinates, suitable for the position of a character in a text.
-type Position = private {
-    _Line: uint64
-    _Column: uint64
-    _Index: CharacterIndex
+type Position = {
+    /// The position's line.
+    Line: uint64
+    /// The position's column.
+    Column: uint64
+    /// The position's character index.
+    Index: uint64
 }
 with
-    /// The position's line.
-    member x.Line = x._Line
-    /// The position's column.
-    member x.Column = x._Column
-    /// The position's character index.
-    member x.Index = x._Index
     override x.ToString() =
         sprintf "(%d, %d)" x.Line x.Column
 
@@ -35,12 +26,12 @@ module Position =
     /// Changes the position according to the given character.
     /// It always increases the index by one, but it takes newlines into account to
     /// change the line and column accordingly.
-    let advance c {_Line = line; _Column = col; _Index = CharacterIndex idx} =
-        let mkPos line col = {_Line = line; _Column = col; _Index = CharacterIndex <| idx + GenericOne}
+    let advance c {Line = line; Column = col; Index = idx} =
+        let mkPos line col = {Line = line; Column = col; Index = idx + GenericOne}
         match c, col with
         | '\n', col when col = GenericOne -> mkPos line col
         | '\n', _ | '\r', _ -> mkPos (line + GenericOne) GenericOne
         | _ -> mkPos line (col + GenericOne)
 
     /// A `Position` that points to the beginning of a stream.
-    let initial = {_Line = GenericOne; _Column = GenericOne; _Index = CharacterIndex GenericZero}
+    let initial = {Line = GenericOne; Column = GenericOne; Index = GenericZero}

042cc4ed25e2129751b820762b0d15c0926bb291
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-01-08 12:17:43 +0200

    Add more functions to create transformers, as well as some ready ones.

--- sample/Farkle.Calculator/SimpleMaths.fs
+++ sample/Farkle.Calculator/SimpleMaths.fs
@@ -80,11 +80,7 @@ let TheRuntimeFarkle =
     // even if they are listed below.
     let transformers =
         [
-            #if NETCOREAPP2_1
-            Transformer.create Symbol.Number <| C Int32.Parse
-            #else
-            Transformer.create Symbol.Number (C(fun data -> Int32.Parse(data.ToString())))
-            #endif
+            Transformer.int Symbol.Number
         ]
     // The fusers merge the parts of a production into one object of your desire.
     // Do not delete anything here, or the post-processor will fail.
--- src/Farkle/PostProcessor/Transformer.fs
+++ src/Farkle/PostProcessor/Transformer.fs
@@ -22,7 +22,8 @@ type Transformer = internal {
     TheTransformer: C2<obj>
 }
 with
-    /// Creates a `Transformer` that transforms the `Terminal` with the given index
+    /// Creates a `Transformer` that transforms the `Terminal`s with the
+    /// given integer index in the grammar according to the given delegate.
     static member Create idx (fTransformer: C2<'TOutput>) =
         {
             SymbolIndex = idx
@@ -33,14 +34,34 @@ with
 /// Functions to create `Transformer`s.
 module Transformer =
 
-    /// Creates a `Transformer` that applies the given delegate to the symbol's data.
-    let inline create sym (fTransform: C<'TOutput>) =
-        Transformer.Create
-            (uint32 sym)
-            (C2(fun _ data -> fTransform.Invoke(data)))
+    /// Creates a `Transformer` that applies the given delegate to the terminal's data and position.
+    let inline createP sym fTransform = Transformer.Create (uint32 sym) fTransform
 
-    /// Creates a `Transformer` that applies the given delegate to the symbol's data and position.
-    let inline createPositionSensitive sym (fTransform: C2<'TOutput>) =
-        Transformer.Create
-            (uint32 sym)
-            fTransform
+    /// Creates a `Transformer` that applies the given delegate to the terminal's data.
+    let inline create sym (fTransform: C<_>) = createP sym (C2(fun _ data -> fTransform.Invoke(data)))
+
+    /// Creates a `Transformer` that applies the given function to the terminal's data as a string, and its position.
+    /// This function will cause an allocation of a string, so if you don't want the string itself
+    /// (or its entirety), it is better to use the delegate-accepting variants.
+    let inline createPS sym fTransform = createP sym (C2(fun pos data -> fTransform pos <| data.ToString()))
+
+    /// Creates a `Transformer` that applies the given function to the terminal's data as a string.
+    /// This function will cause an allocation of a string, so if you don't want the string itself
+    /// (or its entirety), it is better to use the delegate-accepting variants.
+    let inline createS sym fTransform = createPS sym (fun _ data -> fTransform data)
+
+    /// Creates a `Transformer` that transforms a terminal's data to a 32-bit signed integer.
+    let inline int sym =
+        #if NETCOREAPP2_1
+        create sym <| C Int32.Parse
+        #else
+        create sym <| C (fun data -> Int32.Parse(data.ToString()))
+        #endif
+
+    /// Creates a `Transformer` that transforms a terminal's data to a string as-is.
+    let inline string sym = createS sym id
+
+    /// Creates a `Transformer` that ignore's a terminal's data.
+    /// It is better not to include a `Transformer` in a post-processor
+    /// configuration, instead of including an ignoring one.
+    let inline ignore sym = create sym <| C (fun _ -> null)

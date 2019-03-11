4f461b0dc1c4493d3c18ce6c3fa975ae904cb489
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-03-11 22:45:06 +0200

    Make the built-in templates embedded resources. And add a command-line argument to list them.

---
+++ src/Farkle.Tools/BuiltinTemplates.fs
@@ -0,0 +1,34 @@
+// Copyright (c) 2019 Theodore Tsirpanis
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+module Farkle.Tools.BuiltinTemplates
+
+open System
+open System.IO
+open System.Reflection
+open System.Text.RegularExpressions
+
+// The folder had a dash, not an underscore!
+let private builtinsFolder = "builtin_scripts"
+
+let resolveInput x =
+    match x with
+    | ":" -> Console.In
+    | x when x.StartsWith(":") ->
+        let stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(sprintf "Farkle.Tools.%s.%s.scriban" builtinsFolder <| x.Substring(1))
+        new StreamReader(stream) :> TextReader
+    | x -> File.OpenText x :> TextReader
+
+let getAllBuiltins() =
+    let assembly = Assembly.GetExecutingAssembly()
+    let names = assembly.GetManifestResourceNames()
+    let builtInRegex = Regex(@"Farkle\.Tools\." + builtinsFolder + "\.([\w\.]+)\.scriban", RegexOptions.Compiled)
+    names
+    |> Array.choose (fun x ->
+        let res = builtInRegex.Match x
+        if res.Success then
+            Some res.Groups.[1].Value
+        else
+            None)
--- src/Farkle.Tools/CreateSkeleton.fs
+++ src/Farkle.Tools/CreateSkeleton.fs
@@ -11,20 +11,22 @@ open Farkle.Grammar
 open Farkle.Grammar.GOLDParser
 open Scriban
 open Scriban.Runtime
-open System.IO
 open System
+open System.IO
 
 type SkeletonArguments =
     | [<ExactlyOnce; AltCommandLine("-g")>] GrammarFile of string
     | [<ExactlyOnce; AltCommandLine("-t")>] TemplateFile of string
-    | [<AltCommandLine("-o")>] OutputFile of string
+    | [<Unique; AltCommandLine("-o")>] OutputFile of string
+    | [<Unique; AltCommandLine("--list")>] ListTemplates
 with
     interface IArgParserTemplate with
         member x.Usage =
             match x with
             | GrammarFile _ -> "specify the *.egt grammar file to parse. Required."
-            | TemplateFile _ -> "specify the template file to use. Required."
+            | TemplateFile _ -> "specify the template file to use. Required. Use a colon (:) for stdin, and prefix a colon for the built-in templates."
             | OutputFile _ -> "specify where the generated output will be stored. Defaults to the template's name."
+            | ListTemplates -> "list the built-in templates."
 
 type FarkleSkeletonScriptObject(grammarBase64: string, grammar: Grammar) =
     inherit ScriptObject()
@@ -38,24 +40,27 @@ type FarkleSkeletonScriptObject(grammarBase64: string, grammar: Grammar) =
     member __.FileExtension = "." + fileExt
 
 let doSkeleton (args: ParseResults<_>) =
-    let grammarFile = args.GetResult <@ GrammarFile @>
-    let templateFile = args.GetResult <@ TemplateFile @>
-    let outputFile = args.TryGetResult <@ OutputFile @>
-    eprintfn "Creating a skeleton program from %A, based on template %A, to %A..." grammarFile templateFile outputFile
+    if args.Contains <@ ListTemplates @> then
+        BuiltinTemplates.getAllBuiltins() |> Array.iter (printfn "%s")
+    else
+        let grammarFile = args.GetResult <@ GrammarFile @>
+        let templateFile = args.GetResult <@ TemplateFile @>
+        let outputFile = args.TryGetResult <@ OutputFile @>
+        eprintfn "Creating a skeleton program from %A, based on template %A, to %A..." grammarFile templateFile outputFile
 
-    let templateText = File.ReadAllText templateFile
-    let grammarBase64, grammar =
-        let bytes = File.ReadAllBytes grammarFile
-        use mem = new MemoryStream(bytes)
-        let grammar = EGT.ofStream mem |> returnOrFail
-        let grammarBase64 = Convert.ToBase64String bytes
-        grammarBase64, grammar
+        let templateText = BuiltinTemplates.resolveInput(templateFile).ReadToEnd()
+        let grammarBase64, grammar =
+            let bytes = File.ReadAllBytes grammarFile
+            use mem = new MemoryStream(bytes)
+            let grammar = EGT.ofStream mem |> returnOrFail
+            let grammarBase64 = Convert.ToBase64String bytes
+            grammarBase64, grammar
 
-    let template = Template.Parse(templateText, templateFile)
-    let tc = TemplateContext()
-    let so = FarkleSkeletonScriptObject(grammarBase64, grammar)
-    tc.PushGlobal so
-    let output = template.Render(tc)
+        let template = Template.Parse(templateText, templateFile)
+        let tc = TemplateContext()
+        let so = FarkleSkeletonScriptObject(grammarBase64, grammar)
+        tc.PushGlobal so
+        let output = template.Render(tc)
 
-    let outputFile = outputFile |> Option.defaultWith (fun () -> Path.ChangeExtension(grammarFile, so.FileExtension))
-    File.WriteAllText(outputFile, output)
+        let outputFile = outputFile |> Option.defaultWith (fun () -> Path.ChangeExtension(grammarFile, so.FileExtension))
+        File.WriteAllText(outputFile, output)

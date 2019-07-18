86e34215e70ce1157077984197c65bada8e5ac12
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-07-18 20:21:39 +0300

    Avoid double quotes in error messages.

--- src/Farkle/Parser/ParserTypes.fs
+++ src/Farkle/Parser/ParserTypes.fs
@@ -92,16 +92,16 @@ type ParseErrorType =
     | InternalError of InternalError
     override x.ToString() =
         match x with
-        | LexicalError x -> sprintf "Cannot recognize character `%c`" x
+        | LexicalError x -> sprintf "Cannot recognize character '%c'" x
         | SyntaxError (expected, actual) ->
             let expected = expected |> Seq.map string |> String.concat ", "
-            sprintf "Found `%O`, while expecting one of the following tokens: %s" actual expected
-        | CannotNestGroups(g1, g2) -> sprintf "Group `%O` cannot be nested inside `%O`" g1 g2
-        | UnexpectedGroupEnd ge -> sprintf "`%O` was encountered outside of any group." ge
-        | UnexpectedEndOfInput g -> sprintf "Unexpected end of input while being inside a %s." g.Name
-        | TransformError (term, ex) -> sprintf "Exception in user code while post-processing terminal `%O`.\nException:\n%O" term ex
-        | FuseError (prod, ex) -> sprintf "Exception in user code while post-processing production `%O`.\nException:\n%O" prod ex
-        | FuserNotFound prod -> sprintf "Configuration error: No fuser specified for production `%O`.\nYou have to write one, and recompile your program." prod
+            sprintf "Found %O, while expecting one of the following tokens: %s" actual expected
+        | CannotNestGroups(g1, g2) -> sprintf "Group '%s' cannot be nested inside '%s'" g1.Name g2.Name
+        | UnexpectedGroupEnd ge -> sprintf "'%s' was encountered outside of any group." ge.Name
+        | UnexpectedEndOfInput g -> sprintf "Unexpected end of input while being inside group '%s'." g.Name
+        | TransformError (term, ex) -> sprintf "Exception in user code while post-processing terminal '%s'.\nException:\n%O" term.Name ex
+        | FuseError (prod, ex) -> sprintf "Exception in user code while post-processing production '%O'.\nException:\n%O" prod ex
+        | FuserNotFound prod -> sprintf "Configuration error: No fuser specified for production '%O'.\nYou have to write one, and recompile your program." prod
         | InternalError x -> sprintf "Internal error. This is most probably a bug in the parser. If you see this error, please file an issue on GitHub.\nDetails:\n%A" x
 
 /// A log message that contains a position it was encountered.

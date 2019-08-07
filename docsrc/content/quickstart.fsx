f442d522452af529f7e00734093839554bde44fb
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-08-07 16:19:18 +0300

    Change default extension for generated templates to .egt.(c|f)s.

--- docsrc/content/quickstart.fsx
+++ docsrc/content/quickstart.fsx
@@ -47,7 +47,7 @@ Save this file next to the other source files of your project, open the project 
 ``` xml
 <ItemGroup>
   <Farkle Include="SimpleMaths.egt" Namespace="MyBeautifulCalculator" />
-  <Compile Include="SimpleMaths.g.fs" />
+  <Compile Include="SimpleMaths.egt.fs" />
   <!--The rest of your source code files.-->
 </ItemGroup>
 ```

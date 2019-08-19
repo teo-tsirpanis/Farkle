60770e4d18425ca86db2af2232f105fd651f3866
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-08-19 19:00:28 +0300

    Add a predefined sets type. Refactor the predefined sets generator.

--- src/Farkle.Tools/Commands/GeneratePredefinedSets.fs
+++ src/Farkle.Tools/Commands/GeneratePredefinedSets.fs
@@ -40,76 +40,80 @@ type PredefinedSet = {
     Characters: CharacterRange []
 }
 
-let getCategoryFullName =
-    function
-    | "C" -> "Constants"
-    | "M" -> "Miscellaneous"
-    | "A" -> "ASCII Useful"
-    | "U" -> "Unicode Useful"
-    | "B" -> "Unicode Blocks"
-    | x -> x
+module private Implementation =
 
-let private fHeaderCheck x =
-    match x with
-    | "GOLD Character Sets" -> Ok ()
-    | _ -> Error InvalidEGTFile
+    let getCategoryFullName =
+        function
+        | "C" -> "Constants"
+        | "M" -> "Miscellaneous"
+        | "A" -> "ASCII Useful"
+        | "U" -> "Unicode Useful"
+        | "B" -> "Unicode Blocks"
+        | x -> x
 
-let private correctPredefinedSet fAdd x =
-    match x.Name with
-    | "Digit" -> ()
-    | "Euro Sign" -> fAdd {x with Category = "Constants"}
-    | _ -> fAdd x
+    let fHeaderCheck x =
+        match x with
+        | "GOLD Character Sets" -> Ok ()
+        | _ -> Error InvalidEGTFile
 
-let private fRecord (fAdd: _ -> unit) (mem: ReadOnlyMemory<_>) =
-    lengthMustBeAtLeast mem 4
-    let name = wantString mem 0
-    let category = wantString mem 1 |> getCategoryFullName
-    let comment = wantString mem 2
-    Log.Debug("Read character set with name {Name}, category {Category} and comment {Comment}", name, category, comment)
-    let count = wantUInt16 mem 3 |> int
-    lengthMustBe mem <| 4 + 2 * count
-    let mem = mem.Slice(4)
-    let characters = Array.zeroCreate count
-    for i = 0 to count - 1 do
-        let c1 = 2 * i + 0 |> wantUInt16 mem |> char
-        let c2 = 2 * i + 1 |> wantUInt16 mem |> char
-        Array.set characters i {CFrom = c1; CTo = c2}
-    {Name = name; Category = category; Comment = comment; Characters = characters} |> fAdd
+    let correctPredefinedSet fAdd x =
+        match x.Name with
+        // Digit was deprecated by GOLD Parser.
+        | "Digit" -> ()
+        // Euro Sign does not IMO belong to the Miscellaneous category.
+        | "Euro Sign" -> fAdd {x with Category = "Constants"}
+        | _ -> fAdd x
 
-let private readPredefinedSets filePath =
-    use stream = File.OpenRead(filePath)
-    use br = new BinaryReader(stream)
-    let list = ResizeArray()
-    match readEGT fHeaderCheck (fRecord <| correctPredefinedSet list.Add) br with
-    | Ok () ->
-        Log.Debug("Reading {FilePath} succeeded.", filePath)
-        Ok <| list.ToArray()
-    | Error x ->
-        Log.Error("Reading {FilePath} failed: {ErrorType}", x)
-        Error()
+    let fRecord (fAdd: _ -> unit) (mem: ReadOnlyMemory<_>) =
+        lengthMustBeAtLeast mem 4
+        let name = wantString mem 0
+        let category = wantString mem 1 |> getCategoryFullName
+        let comment = wantString mem 2
+        Log.Debug("Read character set with name {Name}, category {Category} and comment {Comment}", name, category, comment)
+        let count = wantUInt16 mem 3 |> int
+        lengthMustBe mem <| 4 + 2 * count
+        let mem = mem.Slice(4)
+        let characters = Array.zeroCreate count
+        for i = 0 to count - 1 do
+            let c1 = 2 * i + 0 |> wantUInt16 mem |> char
+            let c2 = 2 * i + 1 |> wantUInt16 mem |> char
+            Array.set characters i {CFrom = c1; CTo = c2}
+        {Name = name; Category = category; Comment = comment; Characters = characters} |> fAdd
 
-module private Utilities =
+    let readPredefinedSets filePath =
+        use stream = File.OpenRead(filePath)
+        use br = new BinaryReader(stream)
+        let list = ResizeArray()
+        match readEGT fHeaderCheck (fRecord <| correctPredefinedSet list.Add) br with
+        | Ok () ->
+            Log.Debug("Reading {FilePath} succeeded.", filePath)
+            Ok <| list.ToArray()
+        | Error x ->
+            Log.Error("Reading {FilePath} failed: {ErrorType}", x)
+            Error()
 
-    let hexChar (c: char) = sprintf "%04x" (int c)
+    module private Utilities =
 
-    let capitalizeFirst (x: string) =
-        if String.IsNullOrEmpty(x) || Char.IsUpper(x, 0) then
-            x
-        else
-            let sb = StringBuilder()
-            sb.Append(Char.ToUpperInvariant(x.[0])).Append(x, 1, x.Length - 1) |> ignore
-            sb.ToString()
+        let hexChar (c: char) = sprintf "%04x" (int c)
 
-    let makeFSharpIndent (x: string) =
-        x.Split([|' '; '-'|])
-        |> Seq.map capitalizeFirst
-        |> String.concat ""
+        let capitalizeFirst (x: string) =
+            if String.IsNullOrEmpty(x) || Char.IsUpper(x, 0) then
+                x
+            else
+                let sb = StringBuilder()
+                sb.Append(Char.ToUpperInvariant(x.[0])).Append(x, 1, x.Length - 1) |> ignore
+                sb.ToString()
+
+        let makeFSharpIndent (x: string) =
+            x.Split([|' '; '-'|])
+            |> Seq.map capitalizeFirst
+            |> String.concat ""
 
     let loadUtilities(so: ScriptObject) =
-        so.Import("make_fsharp_indent", Func<_,_> makeFSharpIndent)
-        so.Import("hex_char", Func<_,_> hexChar)
+        so.Import("make_fsharp_indent", Func<_,_> Utilities.makeFSharpIndent)
+        so.Import("hex_char", Func<_,_> Utilities.hexChar)
 
-let private template = """// This file was generated from data from the GOLD Parser Builder.
+    let template = """// This file was generated from data from the GOLD Parser Builder.
 
 [<AutoOpen>]
 /// Some common character sets that were imported from GOLD Parser.
@@ -121,15 +125,17 @@ module Farkle.Builder.PredefinedSets
     {{~ end ~}}
     {{~ if s.characters.size == 1 ~}}
     {{~ c = s.characters[0] ~}}
-    let {{ make_fsharp_indent s.name }} = Set.ofRanges ['\u{{ hex_char c.cfrom }}', '\u{{ hex_char c.cto }}']
+    let {{ make_fsharp_indent s.name }} = PredefinedSets.Create "{{s.name}}" ['\u{{ hex_char c.cfrom }}', '\u{{ hex_char c.cto }}']
     {{~ else ~}}
-    let {{ make_fsharp_indent s.name }} = Set.ofRanges [
+    let {{ make_fsharp_indent s.name }} = PredefinedSets.Create "{{s.name}}" [
     {{~ for c in s.characters ~}}
         '\u{{ hex_char c.cfrom }}', '\u{{ hex_char c.cto }}'
     {{~ end ~}}
     ]
     {{~ end ~}}
 {{ end }}"""
+
+open Implementation
 
 let generatePredefinedSets filePath = either {
     let! theSets = readPredefinedSets filePath
@@ -138,7 +144,7 @@ let generatePredefinedSets filePath = either {
     let! template = parseScribanTemplate Log.Logger template "Predefined Sets F# Template"
     let so = ScriptObject()
     so.SetValue("predefined_sets", theSets, true)
-    Utilities.loadUtilities so
+    loadUtilities so
 
     let tc = TemplateContext()
     tc.StrictVariables <- true
--- src/Farkle/Builder/BuilderTypes.fs
+++ src/Farkle/Builder/BuilderTypes.fs
@@ -6,6 +6,8 @@
 namespace Farkle.Builder
 
 open Farkle.Grammar
+open System.Collections
+open System.Collections.Generic
 
 [<RequireQualifiedAccess>]
 /// An error the builder encountered.
@@ -26,3 +28,33 @@ The conflict is caused when two or more terminal definitions can accept the same
             let symbols = xs |> Seq.map DFASymbol.toString |> String.concat ", "
             sprintf "The symbols %s can contain zero characters." symbols
         | NoSymbolsSpecified -> "No symbols were actually specified."
+
+/// A commonly used set of characters.
+type PredefinedSet = private {
+    _Name: string
+    _CharacterRanges: (char * char) list
+    CharactersThunk: Lazy<char Set>
+}
+with
+    static member private CharactersImpl x =
+        Seq.collect (fun (cFrom, cTo) -> {cFrom .. cTo}) x |> set
+    /// Creates a `PredefinedSet` with the specified name and character ranges.
+    static member Create name ranges = {
+        _Name = name
+        _CharacterRanges = ranges
+        CharactersThunk = lazy (PredefinedSet.CharactersImpl ranges)
+    }
+    /// The set's name. Used for informative purposes.
+    member x.Name = x._Name
+    /// A sequence of tuples that show the inclusive ranges of characters that belong to this set.
+    member x.CharacterRanges = Seq.ofList x._CharacterRanges
+    /// The set's characters.
+    member x.Characters = x.CharactersThunk.Value
+    /// The set's character count.
+    member x.Count = x.Characters.Count
+    interface IEnumerable with
+        member x.GetEnumerator() = (x.Characters :> IEnumerable).GetEnumerator()
+    interface IEnumerable<char> with
+        member x.GetEnumerator() = (x.Characters :> IEnumerable<_>).GetEnumerator()
+    interface IReadOnlyCollection<char> with
+        member x.Count = x.Count

c49526955a0bb3c2cf000a717e389b67202bcb30
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-08-25 01:19:27 +0300

    Write more of the DesigntimeFarkle API. Add a reference type that can only be set once.

--- src/Farkle/Builder/DesigntimeFarkle.fs
+++ src/Farkle/Builder/DesigntimeFarkle.fs
@@ -6,28 +6,37 @@
 namespace rec Farkle.Builder
 
 open Farkle
+open Farkle.Common
 open Farkle.Collections
 open System
 open System.Collections.Immutable
 
-type T<'T> = delegate of ReadOnlySpan<char> * Position -> 'T
+type T<'T> = delegate of Position * ReadOnlySpan<char> -> 'T
+
+type DesigntimeFarkle =
+    abstract Name: string
 
 type Terminal =
+    inherit DesigntimeFarkle
     abstract Id: Guid
-    abstract Name: string
+    // abstract Name: string
     abstract Regex: Regex
     abstract Transformer: T<obj>
 
 type Nonterminal =
+    inherit DesigntimeFarkle
     abstract Id: Guid
-    abstract Name: String
-    abstract Parts: Production list
+    // abstract Name: String
+    abstract Productions: Production list
 
 type Production =
     abstract Members: Symbol ImmutableArray
     abstract Fuse: obj [] -> obj
 
 type Symbol = Choice<Terminal, Nonterminal>
+
+type DesigntimeFarkle<'T> = 
+    inherit DesigntimeFarkle
 
 type Terminal<'T> = internal {
     Id: Guid
@@ -36,22 +45,45 @@ type Terminal<'T> = internal {
     Transformer: T<obj> 
 }
 with
+    static member Create name regex (fTransform: T<'T>): Terminal<'T> = {
+        Id = Guid.NewGuid()
+        Name = name
+        Regex = regex
+        Transformer = T(fun pos data -> fTransform.Invoke(pos, data) |> box)
+    }
     interface Terminal with
         member x.Id = x.Id
-        member x.Name = x.Name
+        // member x.Name = x.Name
         member x.Regex = x.Regex
         member x.Transformer = x.Transformer
+    interface DesigntimeFarkle with
+        member x.Name = x.Name
+    interface DesigntimeFarkle<'T>
 
 type Nonterminal<'T> = internal {
     Id: Guid
     Name: string
-    mutable Parts: Production list
+    Productions: SetOnce<Production list>
 }
 with
+    static member Create name = {
+        Id = Guid.NewGuid()
+        Name = name
+        Productions = SetOnce<_>.Create()
+    }
+    member x.SetProductions([<ParamArray>] prods: Production<'T> []) =
+        prods
+        |> Seq.map (fun x -> x :> Production)
+        |> List.ofSeq
+        |> x.Productions.TrySet
+        |> ignore
     interface Nonterminal with
         member x.Id = x.Id
+        // member x.Name = x.Name
+        member x.Productions = x.Productions.ValueOrDefault []
+    interface DesigntimeFarkle with
         member x.Name = x.Name
-        member x.Parts = x.Parts
+    interface DesigntimeFarkle<'T>
 
 type Production<'T> = internal {
     Members: Symbol ImmutableArray
@@ -62,21 +94,22 @@ with
         member x.Members = x.Members
         member x.Fuse arguments = x.Fuse arguments
 
-type Symbol<'T> = Choice<Terminal<'T>, Nonterminal<'T>>
-
-module Symbol =
-    let generalize (x: Symbol<_>): Symbol =
+module private Symbol =
+    let specialize (x: DesigntimeFarkle): Symbol =
         match x with
-        | Choice1Of2 x -> x :> Terminal |> Choice1Of2
-        | Choice2Of2 x -> x :> Nonterminal |> Choice2Of2
+        | :? Terminal as term -> Choice1Of2 term
+        | :? Nonterminal as nont -> Choice2Of2 nont
+        | _ -> failwith "Using a custom derivative of the DesigntimeFarkle interface is not allowed."
+    let append xs df = ImmutableList.add xs (specialize df)
 
 type AbstractProductionBuilder<'T> =
-    abstract DoAppend: Symbol -> 'T
+    abstract Append: DesigntimeFarkle -> 'T
 
+[<Sealed>]
 type ProductionBuilder(members) =
     interface AbstractProductionBuilder<ProductionBuilder> with
-        member __.DoAppend(sym) = ProductionBuilder(ImmutableList.add members sym)
-    member __.Extend(sym: Symbol<'T1>) = ProductionBuilder<'T1>(ImmutableList.add members (Symbol.generalize sym), members.Count)
+        member __.Append(sym) = ProductionBuilder(Symbol.append members sym)
+    member __.Extend(df: DesigntimeFarkle<'T1>) = ProductionBuilder<'T1>(ImmutableList.add members (Symbol.specialize df), members.Count)
     member __.FinishRaw(fFuseRaw: _ -> 'TOutput) : Production<'TOutput> = {
         Members = members.ToImmutableArray()
         Fuse = fFuseRaw >> box
@@ -84,28 +117,44 @@ type ProductionBuilder(members) =
     member x.Finish(fFuseThunk) = x.FinishRaw(fun _ -> fFuseThunk ())
     member x.Finish(fConstant) = x.FinishRaw(fun _ -> fConstant)
 
+[<Sealed>]
 type ProductionBuilder<'T1>(members, idx1) =
     interface AbstractProductionBuilder<ProductionBuilder<'T1>> with
-        member __.DoAppend(sym) = ProductionBuilder<_>(ImmutableList.add members sym, idx1)
-    member __.Extend(sym: Symbol<'T2>) = ProductionBuilder<'T1, 'T2>(ImmutableList.add members (Symbol.generalize sym), idx1, members.Count)
+        member __.Append(sym) = ProductionBuilder<_>(Symbol.append members sym, idx1)
+    member __.Extend(df: DesigntimeFarkle<'T2>) = ProductionBuilder<'T1, 'T2>(ImmutableList.add members (Symbol.specialize df), idx1, members.Count)
     member __.Finish(f: 'T1 -> 'TOutput) : Production<'TOutput> = {
         Members = members.ToImmutableArray()
         Fuse = fun arr -> f (arr.[idx1] :?> _) |> box
     }
 
+[<Sealed>]
 type ProductionBuilder<'T1, 'T2>(members, idx1, idx2) =
     interface AbstractProductionBuilder<ProductionBuilder<'T1, 'T2>> with
-        member __.DoAppend(sym) = ProductionBuilder<_, _>(ImmutableList.add members sym, idx1, idx2)
-    member __.Extend(sym: Symbol<'T3>) = ProductionBuilder<'T1, 'T2, 'T3>(ImmutableList.add members (Symbol.generalize sym), idx1, idx2, members.Count)
+        member __.Append(sym) = ProductionBuilder<_, _>(Symbol.append members sym, idx1, idx2)
+    member __.Extend(df: DesigntimeFarkle<'T3>) = ProductionBuilder<'T1, 'T2, 'T3>(ImmutableList.add members (Symbol.specialize df), idx1, idx2, members.Count)
     member __.Finish(f: 'T1 -> 'T2 -> 'TOutput) : Production<'TOutput> = {
         Members = members.ToImmutableArray()
         Fuse = fun arr -> f (arr.[idx1] :?> _) (arr.[idx2] :?> _) |> box
     }
 
+[<Sealed>]
 type ProductionBuilder<'T1, 'T2, 'T3>(members, idx1, idx2, idx3) =
     interface AbstractProductionBuilder<ProductionBuilder<'T1, 'T2, 'T3>> with
-        member __.DoAppend(sym) = ProductionBuilder<_, _, _>(ImmutableList.add members sym, idx1, idx2, idx3)
+        member __.Append(sym) = ProductionBuilder<_, _, _>(Symbol.append members sym, idx1, idx2, idx3)
     member __.Finish(f: 'T1 -> 'T2 -> 'T3 -> 'TOutput) : Production<'TOutput> = {
         Members = members.ToImmutableArray()
         Fuse = fun arr -> f (arr.[idx1] :?> _) (arr.[idx2] :?> _) (arr.[idx3] :?> _) |> box
     }
+
+module DesigntimeFarkleOperators =
+
+    let (!!=) name parts =
+        let nont = Nonterminal.Create name
+        nont.Productions.TrySet(parts) |> ignore
+        nont
+
+    // https://github.com/ionide/ionide-vscode-fsharp/issues/1203
+    let op_DotGreaterGreater (pb: AbstractProductionBuilder<_>) df = pb.Append df
+
+    let inline op_DotGreaterGreaterDot (rb: ^TBuilder when ^TBuilder : (member Extend : DesigntimeFarkle<_> -> ^TBuilderResult)) df =
+        (^TBuilder : (member Extend: DesigntimeFarkle<_> -> ^TBuilderResult) (rb, df))
--- src/Farkle/Common.fs
+++ src/Farkle/Common.fs
@@ -5,10 +5,11 @@
 
 namespace Farkle.Common
 
+open System
+open System.Threading
+
 /// Faster functions to compare two objects.
 module internal FastCompare =
-
-    open System
 
     /// Compares the first object with another object of the same type.
     /// The types must implement the `IComparable<T>` generic interface.
@@ -21,6 +22,44 @@ module internal FastCompare =
 
     let inline smaller x1 x2 = compare x1 x2 < 0
     let inline smallerOrEqual x1 x2 = compare x1 x2 <= 0
+
+/// A reference type whose value can only be set once.
+type SetOnce< [<ComparisonConditionalOn; EqualityConditionalOn>] 'T> = private {
+    mutable _IsSet: int
+    mutable _Value: 'T
+}
+with
+    /// Tries to set this `SetOnce`'s value to an object.
+    /// Returns whether the value was changed.
+    /// This method is thread-safe.
+    member x.TrySet v =
+        if Interlocked.CompareExchange(&x._IsSet, 1, 0) = 0 then
+            x._Value <- v
+            true
+        else
+            false
+    /// Returns whether this `SetOnce` has a value set.
+    member x.IsSet = x._IsSet <> 0
+    /// Returns the `SetOnce`'s value - if it is set - or the given object otherwise.
+    member x.ValueOrDefault(def) =
+        match x._IsSet with
+        | 0 -> def
+        | _ -> x._Value
+    /// Creates a `SetOnce` object whose value can be set at a later time.
+    /// (try to guess how many times)
+    static member Create() = {
+        _IsSet = 0
+        _Value = Unchecked.defaultof<_>
+    }
+    /// Creates a `SetOnce` object whose value is already set and cannot be changed.
+    static member Create x = {
+        _IsSet = 1
+        _Value = x
+    }
+    override x.ToString() =
+        match x._IsSet with
+        | 0 -> "(not set)"
+        | _ -> x._Value.ToString()
 
 /// Functions to work with the `FSharp.Core.Result` type.
 [<AutoOpen>]

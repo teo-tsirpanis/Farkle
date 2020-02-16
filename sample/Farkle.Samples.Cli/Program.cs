a9de3010f145de74b7d66e7aa80cfd651e8f0873
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-02-16 12:15:54 +0200

    JIT the parser at the start of Farkle.Samples.Cli.

--- sample/Farkle.Samples.Cli/Program.cs
+++ sample/Farkle.Samples.Cli/Program.cs
@@ -18,11 +18,20 @@ namespace Farkle.Samples.Cli
                 f();
         }
 
-        private static readonly string _jsonData = File.ReadAllText("../../tests/resources/generated.json");
+        private const string JsonPath = "../../tests/resources/generated.json";
+        private static string _jsonData;
 
         static void Main()
         {
+            _jsonData = File.ReadAllText(JsonPath);
             Console.WriteLine("This program was made to help profiling Farkle.");
+            
+            Console.WriteLine("JITting the parser...");
+            if (JSON.CSharp.Language.Runtime.Parse(_jsonData).IsError
+                || JSON.FSharp.Language.runtime.Parse(_jsonData).IsError)
+            {
+                throw new Exception("Parsing went wrong...");
+            }
             Execute(() => JSON.CSharp.Language.Runtime.Parse(_jsonData), "Farkle C#");
             Execute(() => JSON.FSharp.Language.runtime.Parse(_jsonData), "Farkle F#");
             Execute(() => FParsec.CharParsers.runParserOnString(Chiron.Parsing.jsonR.Value, null, "generated.json", _jsonData), "Chiron");

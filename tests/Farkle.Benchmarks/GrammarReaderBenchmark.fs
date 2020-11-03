b78f3a2843fa874648dcb7363eacf775eb3e9aa1
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-11-03 19:49:51 +0200

    Benchmark only the grammar creator and the EGTneo reader (of binary data, not Base64).

--- tests/Farkle.Benchmarks/GrammarReaderBenchmark.fs
+++ tests/Farkle.Benchmarks/GrammarReaderBenchmark.fs
@@ -15,23 +15,17 @@ open System.IO
 
 type GrammarReaderBenchmark() =
 
-    let base64EGT =
-        File.ReadAllBytes "gml.egt"
-        |> Convert.ToBase64String
-
-    let base64EGTneo =
+    let egtNeo =
         use stream = new MemoryStream()
         EGT.ofFile "gml.egt" |> EGT.toStreamNeo stream
         stream.ToArray()
-        |> Convert.ToBase64String
 
     let designtime = GOLDMetaLanguage.designtime
 
     [<Benchmark>]
-    member _.Base64EGT() = EGT.ofBase64String base64EGT
-
-    [<Benchmark>]
-    member _.Base64EGTneo() = EGT.ofBase64String base64EGTneo
+    member _.EGTneo() =
+        use stream = new MemoryStream(egtNeo, false)
+        EGT.ofStream stream
 
     [<Benchmark>]
     member _.Build() =
@@ -39,7 +33,3 @@ type GrammarReaderBenchmark() =
         |> DesigntimeFarkleBuild.createGrammarDefinition
         |> DesigntimeFarkleBuild.buildGrammarOnly
         |> returnOrFail
-
-    [<Benchmark>]
-    member _.BuildPrecompiled() =
-        RuntimeFarkle.buildUntyped GOLDMetaLanguage.designtime

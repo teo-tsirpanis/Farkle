c0cd55607065a6276d8bc72be006a84e4ab19c12
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-12-07 12:35:31 +0200

    Cache a default tokenizer instance per grammar.

--- src/Farkle/Parser/TokenizerFactory.fs
+++ src/Farkle/Parser/TokenizerFactory.fs
@@ -7,18 +7,26 @@ namespace Farkle.Parser
 
 open Farkle.Grammar
 open System
+open System.Runtime.CompilerServices
 
 /// <summary>A class that creates <see cref="Tokenizer"/>s.</summary>
+[<AbstractClass>]
 type TokenizerFactory() =
-    static let instance = TokenizerFactory()
+    static let createTokenizerCallback =
+        ConditionalWeakTable<_,Tokenizer>.CreateValueCallback(fun grammar -> DefaultTokenizer grammar :> _)
+    // DefaultTokenizers are stateless and therefore
+    // thread-safe. They can be made singletons.
+    static let tokenizerCache = ConditionalWeakTable()
+    static let defaultFactory =
+        {new TokenizerFactory() with
+            member _.CreateTokenizer grammar = tokenizerCache.GetValue(grammar, createTokenizerCallback)}
     /// <summary>Creates a <see cref="Tokenizer"/>.</summary>
     /// <param name="grammar">The <see cref="Grammar"/>
     /// for which the tokenizer is created.</param>
     abstract CreateTokenizer: grammar: Grammar -> Tokenizer
-    default _.CreateTokenizer grammar = Tokenizer(grammar)
     /// <summary>A <see cref="TokenizerFactory"/> object that
     /// creates the default <see cref="Tokenizer"/>.</summary>
-    static member internal Default = instance
+    static member internal Default = defaultFactory
 
 [<Sealed>]
 type internal TokenizerFactoryOfType(tokenizerType: Type) =

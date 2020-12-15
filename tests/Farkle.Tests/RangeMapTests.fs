teo-tsirpanis/Farkle

commit 6be6570dec3a4ccf8d22a3d686ed6932ea2543c0

Use a more lightweight hash code for the bit set.
Use a fully inline implementation of RangeMap.(of/to)Seq, and define ad-hoc separate non-inline functions.
It mimics how Terminals.generic(Un)Signed is implemented.
And also use EqualityComparer.Default instead of (=) in RangeMap.ofSeq.

src/Farkle/Collections/RangeMap.fs
@@ -121,14 +121,20 @@ module RangeMap =
 
     /// Creates a `RangeMap` from a sequence of key-value pairs.
     /// The keys can be of any type that can has the notion of "one" and equality checking.
-    let ofSeqEx fSuccessor xs =
+    // This function can be used from F# like this:
+    // let ofSeq xs = ofSeqEx
+    // let rangeMap =
+    //     mySequence
+    //     |> Seq.map f
+    //     |> ofSeq
+    let inline ofSeqEx xs =
         if not <| Seq.isEmpty xs then
             let xs = Seq.sortBy (fun (KeyValue(k, _)) -> k) xs
             let (KeyValue(k, v)) = Seq.last xs
             Seq.foldBack (fun (KeyValue(k', v')) (kFrom, kTo, v, xs) ->
-                if v = v' && kFrom = fSuccessor k' then
+                if EqualityComparer.Default.Equals(v, v') && EqualityComparer.Default.Equals(kFrom, k + LanguagePrimitives.GenericOne) then
                     (k', kTo, v, xs)
-                elif kFrom <> k' then
+                elif not (EqualityComparer.Default.Equals(kFrom, k')) then
                     (k', k', v', (kFrom, kTo, v) :: xs)
                 else
                     (kFrom, kTo, v, xs))
@@ -138,12 +144,7 @@ module RangeMap =
         else
             RangeMap.Empty
 
-    let inline ofSeq xs = ofSeqEx ((+) LanguagePrimitives.GenericOne) xs
-
-    let toSeqEx (fRange: _ -> _ -> 'key seq) (rm: RangeMap<'key, _>) =
+    let inline toSeqEx (rm: RangeMap<'key, _>) =
         rm
         |> Seq.collect (fun {KeyFrom = kFrom; KeyTo = kTo; Value = v} ->
-            fRange kFrom kTo
-            |> Seq.map (fun k -> KeyValuePair(k, v)))
-
-    let inline toSeq xs = toSeqEx (fun kFrom kTo -> seq {kFrom .. kTo}) xs
+            Seq.map (fun k -> KeyValuePair(k, v)) {kFrom .. kTo})

tests/Farkle.Tests/RangeMapTests.fs
@@ -9,6 +9,10 @@ open Expecto
 open Farkle.Collections
 open Farkle.Tests
 
+let ofSeq xs = RangeMap.ofSeqEx xs
+
+let toSeq xs = RangeMap.toSeqEx xs
+
 [<Tests>]
 let tests =
     testList "RangeMap tests" [
@@ -36,9 +40,9 @@ let tests =
 
         // A boolean value type makes continuous spaces more likely.
         testProperty "RangeMap.ofSeq is the inverse of RangeMap.toSeq" (fun (x: RangeMap<int, bool>) ->
-            let xs = RangeMap.toSeq x
-            let x' = RangeMap.ofSeq xs
-            let xs' = RangeMap.toSeq x'
+            let xs = toSeq x
+            let x' = ofSeq xs
+            let xs' = toSeq x'
             // We have to compare the sequences, not the RangeMaps, because
             // RangeMap.ofSeq might create an equivalent RangeMap with less elements.
             Expect.sequenceEqual xs xs' "The RangeMaps are different")

src/Farkle/Builder/DFABuild.fs
@@ -273,6 +273,8 @@ with
         AnythingElse = None
     }
 
+let createRangeMap xs = RangeMap.ofSeqEx xs
+
 let internal makeDFA
     prioritizeFixedLengthSymbols regex (leaves: RegexBuildLeaves) (followPos: ImmutableArray<BitSet>) =
     let states = Dictionary()
@@ -325,7 +327,7 @@ let internal makeDFA
             // and rarely more than two.
             |> Seq.sortBy snd
             |> List.ofSeq
-        let edges = RangeMap.ofSeq state.Edges
+        let edges = createRangeMap state.Edges
         let rec getAcceptSymbol xs =
             match xs with
             | [] -> Ok None

src/Farkle/Collections/BitSet.fs
@@ -163,7 +163,14 @@ type BitSet private(data: BitField, extra: BitField []) =
         match x' with
         | :? BitSet as x' -> BitSet.AreEqual(&x, &x')
         | _ -> false
-    override _.GetHashCode() = hash data ^^^ hash extra
+    override _.GetHashCode() =
+        /// TODO: Use System.HashCode.
+        let mutable hash = 17
+        hash <- hash * 31 + data.GetHashCode()
+        hash <- hash * 31 + extra.Length.GetHashCode()
+        for i = 0 to extra.Length - 1 do
+            hash <- hash * 31 + extra.[i].GetHashCode()
+        hash
     override _.ToString() =
         let sb = StringBuilder()
         sb.Append(data.ToString("X16")) |> ignore

src/Farkle/Parser/OptimizedOperations.fs
@@ -48,6 +48,8 @@ module private OptimizedOperations =
             arr.[i] <- Array.zeroCreate length2
         arr
 
+    let private rangeMapToSeq xs = RangeMap.toSeqEx xs
+
     /// Creates a two-dimensional array of DFA state indices, whose first dimension
     /// represents the index of the current DFA state, and the second represents the
     /// ASCII character that was encountered.
@@ -59,7 +61,7 @@ module private OptimizedOperations =
             for j = 0 to ASCIIUpperBound + 1 do
                 arr.[i].[j] <- anythingElse
             state.Edges
-            |> RangeMap.toSeq
+            |> rangeMapToSeq
             |> Seq.takeWhile (fun x -> isASCII x.Key)
             |> Seq.iter (fun x ->
                 arr.[i].[int x.Key] <- DFAStateTag.FromOption x.Value))

commit f569ed4289e738713bf7871a4ec51445b0810009

Fix the broken RangeMap.ofSeqEx.
It wasn't actually compacting the consecutive keys; the killer feature of the range map, becoming in essence a glorified array.
This serious bug was first introduced in 6be6570dec3a4ccf8d22a3d686ed6932ea2543c0, in late July 2020, because of a missing single quote from a name.
It was fixed, the variable names became more clear, the implementation was overall optimized, and a test was added to verify the RangeMap's correct behavior.

src/Farkle/Collections/RangeMap.fs
@@ -115,6 +115,8 @@ type RangeMap<'key,'a when 'key :> IComparable<'key>> private(arr: RangeMapEleme
 /// Do not use it; it is subject to be removed or altered at any time.
 module RangeMap =
 
+    open LanguagePrimitives
+
     let ofGroupedRanges xs =
         xs
         |> Seq.collect (fun (keys, value) ->
@@ -131,24 +133,27 @@ module RangeMap =
     //     |> Seq.map f
     //     |> ofSeq
     let inline ofSeqEx xs =
-        if not <| Seq.isEmpty xs then
-            let xs = Seq.sortBy (fun (KeyValue(k, _)) -> k) xs
-            let (KeyValue(k, v)) = Seq.last xs
-            Seq.foldBack (fun (KeyValue(k', v')) (kFrom, kTo, v, xs) ->
-                if EqualityComparer.Default.Equals(v, v') && EqualityComparer.Default.Equals(kFrom, k + LanguagePrimitives.GenericOne) then
-                    (k', kTo, v, xs)
-                elif not (EqualityComparer.Default.Equals(kFrom, k')) then
-                    (k', k', v', (kFrom, kTo, v) :: xs)
+        let xs = Array.ofSeq xs
+        if not <| Array.isEmpty xs then
+            Array.sortInPlaceBy (fun (KeyValue(k, _)) -> k) xs
+            let (KeyValue(kLast, vLast)) = Array.last xs
+            Array.foldBack (fun (KeyValue(kCurrent, vCurrent)) (kFrom, kTo, vExisting, xs) ->
+                if EqualityComparer.Default.Equals(vExisting, vCurrent)
+                    && EqualityComparer.Default.Equals(kFrom, kCurrent + GenericOne) then
+                    (kCurrent, kTo, vExisting, xs)
+                elif not (EqualityComparer.Default.Equals(kFrom, kCurrent)) then
+                    (kCurrent, kCurrent, vCurrent, (kFrom, kTo, vExisting) :: xs)
                 else
-                    (kFrom, kTo, v, xs))
-                xs (k, k, v, [])
+                    (kFrom, kTo, vExisting, xs))
+                xs (kLast, kLast, vLast, [])
             |> (fun (kFrom, kTo, v, xs) -> (kFrom, kTo, v) :: xs)
             |> RangeMap
         else
             RangeMap.Empty
 
     [<NoDynamicInvocation>]
-    let inline toSeqEx (rm: RangeMap<'key, _>) =
-        rm
-        |> Seq.collect (fun {KeyFrom = kFrom; KeyTo = kTo; Value = v} ->
-            Seq.map (fun k -> KeyValuePair(k, v)) {kFrom .. kTo})
+    let inline toSeqEx (rm: RangeMap<'key, _>) = seq {
+        for {KeyFrom = kFrom; KeyTo = kTo; Value = v} in rm do
+            for k in {kFrom .. kTo} do
+                yield KeyValuePair(k, v)
+    }

tests/Farkle.Tests/RangeMapTests.fs
@@ -8,6 +8,7 @@ module Farkle.Tests.RangeMapTests
 open Expecto
 open Farkle.Collections
 open Farkle.Tests
+open System.Collections.Generic
 
 let ofSeq xs = RangeMap.ofSeqEx xs
 
@@ -46,4 +47,10 @@ let tests =
             // We have to compare the sequences, not the RangeMaps, because
             // RangeMap.ofSeq might create an equivalent RangeMap with less elements.
             Expect.sequenceEqual xs xs' "The RangeMaps are different")
+
+        test "The RangeMap's constructor compacts consecutive keys with the same value" {
+            let rm = [0 .. 100] |> Seq.map(fun x -> KeyValuePair(x, true)) |> ofSeq
+            let expected = [{KeyFrom = 0; KeyTo = 100; Value = true}]
+            Expect.sequenceContainsOrder rm expected "The range map is not compacted"
+        }
     ]
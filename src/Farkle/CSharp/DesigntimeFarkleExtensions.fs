dfed92ebb27484c52f75e75267604469057dbd9e
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2021-07-30 15:11:43 +0300

    Make the C# designtime Farkle metadata setters generic.

--- src/Farkle/CSharp/DesigntimeFarkleExtensions.fs
+++ src/Farkle/CSharp/DesigntimeFarkleExtensions.fs
@@ -29,7 +29,7 @@ type ProductionBuilderExtensions =
     static member AsIs(df) = (!@ df).AsIs()
 
 [<Extension; AbstractClass; Sealed>]
-/// <summary>Extension methods for the <see cref="DesigntimeFarkle{TResult}"/> type.</summary>
+/// <summary>Extension methods for <see cref="DesigntimeFarkle"/>s.</summary>
 /// <remarks>Most of these methods must be applied to the topmost designtime Farkle
 /// that will eventually be built. Use <see cref="Cast"/> first to use these functions
 /// on untyped designtime Farkles.</remarks>
@@ -42,13 +42,12 @@ type DesigntimeFarkleExtensions =
     [<Extension>]
     /// <summary>Casts a <see cref="DesigntimeFarkle"/>
     /// into a <see cref="DesigntimeFarkle{Object}"/></summary>
-    /// <remarks>Useful for setting metadata to untyped designtime Farkles.
-    /// The object <paramref name="df"/> will return is undefined.</remarks>
+    /// <remarks>The object <paramref name="df"/> will return is undefined.</remarks>
     static member Cast df: [<Nullable(1uy, 2uy)>] _ = DesigntimeFarkle.cast df
     [<Extension>]
     /// <summary>Changes the name of a <see cref="DesigntimeFarkle{TResult}"/>.</summary>
     /// <remarks>Useful for diagnostic purposes.</remarks>
-    static member Rename<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, name) =
+    static member Rename<'T when 'T :> DesigntimeFarkle>(df: 'T, name) =
         DesigntimeFarkle.rename name df
     [<Extension>]
     /// <summary>Builds a <see cref="DesigntimeFarkle{TResult}"/>
@@ -79,43 +78,44 @@ type DesigntimeFarkleExtensions =
         RuntimeFarkle.CreateMaybe RuntimeFarkle.syntaxCheckerObj grammar
     [<Extension>]
     /// <summary>Sets a custom <see cref="GrammarMetadata"/>
-    /// object to a typed designtime Farkle.</summary>
+    /// object to a <see cref="DesigntimeFarkle"/>.</summary>
+    /// <seealso cref="WithOperatorScope"/>
     /// <seealso cref="CaseSensitive"/>
     /// <seealso cref="AutoWhitespace"/>
     /// <seealso cref="AddNoiseSymbol"/>
     /// <seealso cref="AddLineComment"/>
     /// <seealso cref="AddBlockComment"/>
-    static member WithMetadata (df: DesigntimeFarkle<'TResult>, metadata) =
+    static member WithMetadata<'T when 'T :> DesigntimeFarkle>(df: 'T, metadata) =
         DesigntimeFarkle.withMetadata metadata df
     [<Extension>]
     /// <summary>Sets an <see cref="Farkle.Builder.OperatorPrecedence.OperatorScope"/>
-    /// to a typed designtime Farkle. Any previous ones -if exist- are discarded.</summary>
-    static member WithOperatorScope<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, opScope) =
+    /// to a <see cref="DesigntimeFarkle"/>. Any previous ones -if exist- are discarded.</summary>
+    static member WithOperatorScope<'T when 'T :> DesigntimeFarkle>(df: 'T, opScope) =
         DesigntimeFarkle.withOperatorScope opScope df
     [<Extension>]
-    /// <summary>Controls whether the given <see cref="DesigntimeFarkle{TResult}"/>
+    /// <summary>Controls whether the given <see cref="DesigntimeFarkle"/>
     /// is case sensitive.</summary>
-    static member CaseSensitive<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, [<Optional; DefaultParameterValue(true)>] x) =
+    static member CaseSensitive<'T when 'T :> DesigntimeFarkle>(df: 'T, [<Optional; DefaultParameterValue(true)>] x) =
         DesigntimeFarkle.caseSensitive x df
     [<Extension>]
-    /// <summary>Controls whether the given <see cref="DesigntimeFarkle{TResult}"/>
+    /// <summary>Controls whether the given <see cref="DesigntimeFarkle"/>
     /// would automatically ignore whitespace in input text.</summary>
-    static member AutoWhitespace<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, x) =
+    static member AutoWhitespace<'T when 'T :> DesigntimeFarkle>(df: 'T, x) =
         DesigntimeFarkle.autoWhitespace x df
     [<Extension>]
     /// <summary>Adds a symbol specified by the given <see cref="Regex"/>
-    /// that will be ignored in input text.</summary>
-    static member AddNoiseSymbol<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, name, regex) =
+    /// that will be ignored in input text, to the given <see cref="DesigntimeFarkle"/>.</summary>
+    static member AddNoiseSymbol<'T when 'T :> DesigntimeFarkle>(df: 'T, name, regex) =
         DesigntimeFarkle.addNoiseSymbol name regex df
     [<Extension>]
-    /// <summary>Adds a new line comment in the given
-    /// <see cref="DesigntimeFarkle{TResult}"/>.</summary>
-    static member AddLineComment<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, commentStart) =
+    /// <summary>Adds a line comment to the given
+    /// <see cref="DesigntimeFarkle"/>.</summary>
+    static member AddLineComment<'T when 'T :> DesigntimeFarkle>(df: 'T, commentStart) =
         DesigntimeFarkle.addLineComment commentStart df
     [<Extension>]
-    /// <summary>Adds a new block comment in the given
-    /// <see cref="DesigntimeFarkle{TResult}"/>.</summary>
-    static member AddBlockComment<[<Nullable(0uy)>] 'TResult>(df: DesigntimeFarkle<'TResult>, commentStart, commentEnd) =
+    /// <summary>Adds a block comment to the given
+    /// <see cref="DesigntimeFarkle"/>.</summary>
+    static member AddBlockComment<'T when 'T :> DesigntimeFarkle>(df: 'T, commentStart, commentEnd) =
         DesigntimeFarkle.addBlockComment commentStart commentEnd df
     [<Extension>]
     /// <summary>Creates a new <see cref="DesigntimeFarkle{TResult}"/>

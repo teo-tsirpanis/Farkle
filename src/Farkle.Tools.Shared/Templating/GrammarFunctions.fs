fc35b376c781df53772baeee0620699455e1af58
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2021-08-09 01:24:32 +0300

    Refactor the GrammarFunctions type into a class hierarchy to be reused in error reports.

--- src/Farkle.Tools.Shared/Templating/GrammarFunctions.fs
+++ src/Farkle.Tools.Shared/Templating/GrammarFunctions.fs
@@ -9,6 +9,7 @@ open Farkle.Grammar
 open Farkle.Tools
 open Scriban.Runtime
 open System
+open System.Collections.Immutable
 open System.IO
 open System.Linq
 open System.Text
@@ -19,7 +20,8 @@ type IdentifierTypeCase =
     | CamelCase
     | PascalCase
 
-type GrammarFunctions(g) =
+[<AbstractClass>]
+type GrammarFunctionsBase(grammarObj: obj) =
 
     static let toIdentifier name case (separator: string) =
         let sb = StringBuilder()
@@ -95,15 +97,48 @@ type GrammarFunctions(g) =
             prod.Head, handle
         isElementUnique getFormattingElements productions
 
-    let grammar = g.Grammar
-    let grammarSO =
+    static let grammarMemberFilter = MemberFilterDelegate(fun mi ->
+        match mi.Name with
+        | "Properties"
+        | "StartSymbol"
+        | "Symbols"
+        | "Productions"
+        | "Groups"
+        | "LALRStates"
+        | "DFAStates" -> true
+        | _ -> false)
+
+    let productions, grammarSO =
         let so = ScriptObject()
-        so.Import grammar
-        so.SetValue("productions_groupped", grammar.Productions.ToLookup(fun x -> x.Head), true)
-        so
+        so.Import(grammarObj, filter = grammarMemberFilter)
+        let productions = so.["productions"] :?> Production ImmutableArray
+        so.SetValue("productions_groupped", productions.ToLookup(fun x -> x.Head), true)
+        productions, so
 
-    let fShouldPrintFullProduction = shouldPrintFullProduction grammar.Productions
+    let fShouldPrintFullProduction = shouldPrintFullProduction productions
+    static member upper_case = UpperCase
+    static member lower_case = LowerCase
+    static member pascal_case = PascalCase
+    static member camel_case = CamelCase
 
+    member _.Grammar = grammarSO
+
+    member _.fmt (x: obj) case separator =
+        match x with
+        | :? Terminal as x -> match x with Terminal(_, name) -> toIdentifier name case separator
+        | :? Production as x -> formatProduction (fShouldPrintFullProduction x) x case separator
+        | _ -> invalidArg "x" (sprintf "Can only format terminals and productions, but got %O instead." <| x.GetType())
+    static member group_dfa_edges {Edges = edges} =
+        edges.ToLookup(fun x -> x.Value).OrderBy(fun x -> x.Key)
+    abstract has_errors: bool
+    abstract LoadInstanceMethods: ScriptObject -> unit
+    default x.LoadInstanceMethods so =
+        so.Import("fmt", Func<_,_,_,_> x.fmt)
+
+type GrammarFunctions(g) =
+    inherit GrammarFunctionsBase(g.Grammar)
+
+    let grammar = g.Grammar
     let bytesThunk = lazy(
         let ext = Path.GetExtension(g.GrammarPath).AsSpan()
         if isGrammarExtension ext then
@@ -114,25 +149,12 @@ type GrammarFunctions(g) =
             stream.ToArray()
     )
 
-    static member upper_case = UpperCase
-    static member lower_case = LowerCase
-    static member pascal_case = PascalCase
-    static member camel_case = CamelCase
-
-    member _.Grammar = grammarSO
     member _.grammar_path = g.GrammarPath
-
-    member _.fmt (x: obj) case separator =
-        match x with
-        | :? Terminal as x -> match x with Terminal(_, name) -> toIdentifier name case separator
-        | :? Production as x -> formatProduction (fShouldPrintFullProduction x) x case separator
-        | _ -> invalidArg "x" (sprintf "Can only format terminals and productions, but got %O instead." <| x.GetType())
     member _.to_base64 doPad =
         let options = if doPad then Base64FormattingOptions.InsertLineBreaks else Base64FormattingOptions.None
         Convert.ToBase64String(bytesThunk.Value, options)
 
-    member x.LoadInstanceMethods(so: ScriptObject) =
-        so.Import("fmt", Func<_,_,_,_> x.fmt)
+    override _.has_errors = false
+    override x.LoadInstanceMethods so =
+        base.LoadInstanceMethods so
         so.Import("to_base64", Func<_,_> x.to_base64)
-    static member group_dfa_edges {Edges = edges} =
-        edges.ToLookup(fun x -> x.Value).OrderBy(fun x -> x.Key)

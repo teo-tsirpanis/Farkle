ca3a0a02d33d3c36910d50cbdbefba21faca700d
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-03-05 21:17:54 +0200

    Remove the super-unsafe ListBuilder implementation.
    We are firmly in F# 6.0 now.

--- src/Farkle/Collections/ListBuilder.fs
+++ src/Farkle/Collections/ListBuilder.fs
@@ -8,12 +8,7 @@ namespace Farkle.Collections
 open System
 open System.Collections
 open System.Collections.Generic
-open System.Runtime.CompilerServices
-
-[<Sealed>]
-type private MutableListWrapper<'T> private() =
-    [<DefaultValue>] val mutable head: 'T
-    [<DefaultValue>] val mutable tail: 'T list
+open Microsoft.FSharp.Core.CompilerServices
 
 // Efficiently builds an F# list by adding elements to its end.
 // Minimally implements the ICollection interface just to be usable
@@ -21,37 +16,19 @@ type private MutableListWrapper<'T> private() =
 // https://github.com/krauthaufen/DawnSharp/blob/master/src/Armadillo/Tools/ListBuilder.fs.
 [<Sealed>]
 type internal ListBuilder<'T>() =
-    static let unsafeSetTail (x: 'T list) (tail: 'T list) =
-        if obj.ReferenceEquals(x, ([]: 'T list)) then
-            invalidOp "Attempting to mutate the empty list singleton. Please report it on GitHub."
-        Unsafe.As<MutableListWrapper<'T>>(x).tail <- tail
-    let mutable head = []
-    let mutable tail = head
+    let mutable builder = ListCollector<'T>()
     static let moveToListFunc = Func<ListBuilder<'T>,_>(fun xs -> xs.MoveToList())
-    member this.MoveToList() =
-        let list = head
-        (this :> ICollection<_>).Clear()
-        list
+    member this.MoveToList() = builder.Close()
     static member MoveToListDelegate = moveToListFunc
     interface IEnumerable with
-        member _.GetEnumerator() = (head :> IEnumerable).GetEnumerator()
+        member _.GetEnumerator() = raise (NotImplementedException())
     interface IEnumerable<'T> with
-        member _.GetEnumerator() = (head :> _ seq).GetEnumerator()
+        member _.GetEnumerator() = raise (NotImplementedException())
     interface ICollection<'T> with
-        member _.Count = head.Length
+        member _.Count = raise (NotImplementedException())
         member _.IsReadOnly = false
-        member _.Add x =
-            let newTail = [x]
-            match head with
-            | [] ->
-                head <- newTail
-                tail <- head
-            | _ ->
-                unsafeSetTail tail newTail
-                tail <- newTail
-        member _.Clear() =
-            head <- []
-            tail <- head
+        member _.Add x = builder.Add x
+        member _.Clear() = builder <- Unchecked.defaultof<_>
         member _.Contains _ = raise (NotImplementedException())
         member _.CopyTo(_, _) = raise (NotImplementedException())
         member _.Remove _ = raise (NotImplementedException())

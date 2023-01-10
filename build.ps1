7fbdc69df1a56fe11298f5e47b1b20f86b0ddf06
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-01-10 21:12:42 +0200

    Remove Paket.
    _Finally._ It won't be missed.

--- build.ps1
+++ build.ps1
@@ -2,6 +2,4 @@ function Test-ExitCode () {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
 
 dotnet tool restore
 Test-ExitCode
-dotnet paket restore
-Test-ExitCode
 dotnet run --project eng/Farkle.Build.fsproj -- @args

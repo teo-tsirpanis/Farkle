6c1b58cf619db4c72c865b223422051b424fa467
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-01-15 16:10:38 +0200

    Use file-scoped namespaces.

--- tests/Farkle.Tests.CSharp/BufferReaderTests.cs
+++ tests/Farkle.Tests.CSharp/BufferReaderTests.cs
@@ -3,47 +3,46 @@
 
 using Farkle.Buffers;
 
-namespace Farkle.Tests.CSharp
+namespace Farkle.Tests.CSharp;
+
+public class BufferReaderTests
 {
-    public class BufferReaderTests
+    [Test]
+    public void TestReadNumbers()
     {
-        [Test]
-        public void TestReadNumbers()
-        {
-            var br = new BufferReader(new byte[] { 184, 0, 219, 1 });
-            Assert.That(br.ReadUInt16(), Is.EqualTo((ushort)184));
-            Assert.That(br.ReadUInt16(), Is.EqualTo((ushort)475));
-        }
+        var br = new BufferReader(new byte[] { 184, 0, 219, 1 });
+        Assert.That(br.ReadUInt16(), Is.EqualTo((ushort)184));
+        Assert.That(br.ReadUInt16(), Is.EqualTo((ushort)475));
+    }
 
-        [Test]
-        public void TestIncompleteBuffers()
-        {
-            var br = new BufferReader(stackalloc byte[16]);
-            Assert.That(br.ReadBytes(10).Length, Is.EqualTo(10));
-            Assert.That(br.Buffer.Length, Is.EqualTo(6));
-            Assert.That(br.ReadBytes(10).Length, Is.EqualTo(6));
-            Assert.That(br.Buffer.IsEmpty);
-        }
+    [Test]
+    public void TestIncompleteBuffers()
+    {
+        var br = new BufferReader(stackalloc byte[16]);
+        Assert.That(br.ReadBytes(10).Length, Is.EqualTo(10));
+        Assert.That(br.Buffer.Length, Is.EqualTo(6));
+        Assert.That(br.ReadBytes(10).Length, Is.EqualTo(6));
+        Assert.That(br.Buffer.IsEmpty);
+    }
 
-        [Test]
-        public void TestAdvanceBy()
+    [Test]
+    public void TestAdvanceBy()
+    {
+        var br = new BufferReader(stackalloc byte[16]);
+        Assert.That(br.Buffer.Length, Is.EqualTo(16));
+        br.AdvanceBy(10);
+        Assert.That(br.Buffer.Length, Is.EqualTo(6));
+
+        bool threw = false;
+        try
         {
-            var br = new BufferReader(stackalloc byte[16]);
-            Assert.That(br.Buffer.Length, Is.EqualTo(16));
             br.AdvanceBy(10);
-            Assert.That(br.Buffer.Length, Is.EqualTo(6));
-
-            bool threw = false;
-            try
-            {
-                br.AdvanceBy(10);
-            }
-            catch (Exception e)
-            {
-                Assert.That(e, Is.InstanceOf<EndOfStreamException>());
-                threw = true;
-            }
-            Assert.That(threw);
         }
+        catch (Exception e)
+        {
+            Assert.That(e, Is.InstanceOf<EndOfStreamException>());
+            threw = true;
+        }
+        Assert.That(threw);
     }
 }

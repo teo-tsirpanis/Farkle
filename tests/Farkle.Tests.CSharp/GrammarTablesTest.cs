91c1e489f63949a3e50828e372acdb13a4f10955
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-01-19 19:46:20 +0200

    Start writing a rundimentary grammar tables writer.
    And it was at this moment that I knew ArrayBufferWriter does not exist on .NET Standard 2.0 and I have to write my own buffer writer now.

--- tests/Farkle.Tests.CSharp/GrammarTablesTest.cs
+++ tests/Farkle.Tests.CSharp/GrammarTablesTest.cs
@@ -1,6 +1,9 @@
 // Copyright © Theodore Tsirpanis and Contributors.
 // SPDX-License-Identifier: MIT
 
+using System.Buffers;
+using Farkle.Buffers;
+using Farkle.Grammars;
 using FsCheck;
 using FsCheck.Fluent;
 
@@ -28,5 +31,58 @@ internal class GrammarTablesTest
             productionMember, stateMachine, specialName, smallBlobHeap, smallStringHeap);
 
     private sealed record TableCounts(int TokenSymbol, int Group, int GroupNesting, int Nonterminal, int Production,
-        int ProductionMember, int StateMachine, int SpecialName, bool SmallBlobHeap, bool SmallStringHeap);
+        int ProductionMember, int StateMachine, int SpecialName, bool SmallBlobHeap, bool SmallStringHeap)
+    {
+        public TableKinds GetTablesPresent()
+        {
+            TableKinds presentTables = TableKinds.Grammar;
+            Update(TokenSymbol, TableKinds.TokenSymbol);
+            Update(Group, TableKinds.Group);
+            Update(GroupNesting, TableKinds.GroupNesting);
+            Update(Nonterminal, TableKinds.Nonterminal);
+            Update(Production, TableKinds.Production);
+            Update(ProductionMember, TableKinds.ProductionMember);
+            Update(StateMachine, TableKinds.StateMachine);
+            Update(SpecialName, TableKinds.SpecialName);
+
+            return presentTables;
+
+            void Update(int count, TableKinds tableKind)
+            {
+                if (count > 0) presentTables |= tableKind;
+            }
+        }
+
+        public byte GetHeapSizes() => (byte)((SmallStringHeap ? 1 : 0) | (SmallBlobHeap ? 2 : 0));
+
+        public byte[] ToArray()
+        {
+            using var bw = new PooledSegmentBufferWriter<byte>();
+
+            TableKinds tablesPresent = GetTablesPresent();
+            int tableCount = PopCount((ulong)tablesPresent);
+            byte heapSizes = GetHeapSizes();
+
+            bw.Write((ulong)tablesPresent);
+
+            return bw.ToArray();
+        }
+
+        // We can't easily use the Compatibility classes
+        // because we are also importing Farkle's, so we
+        // have to write our own implementation.
+        static int PopCount(ulong value)
+        {
+            int count = 0;
+            while (value > 0)
+            {
+                if (value % 2 == 0)
+                {
+                    count++;
+                }
+                value /= 2;
+            }
+            return count;
+        }
+    }
 }

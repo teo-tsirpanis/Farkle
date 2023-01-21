064d0c244037453eee8838c2cf416e2eb2dd084c
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-01-22 00:05:26 +0200

    Add string and blob handle types.

---
+++ src/FarkleNeo/Grammars/BlobHandle.cs
@@ -0,0 +1,50 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+namespace Farkle.Grammars;
+
+/// <summary>
+/// Represents a reference to a blob of bytes in a <see cref="Grammar"/>.
+/// </summary>
+public readonly struct BlobHandle : IEquatable<BlobHandle>
+{
+    internal uint Value { get; }
+
+    internal BlobHandle(uint value) => Value = value;
+
+    /// <summary>
+    /// Whether this <see cref="BlobHandle"/> points to the empty blob.
+    /// </summary>
+    public bool IsNil => Value == 0;
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is BlobHandle handle && Equals(handle);
+
+    /// <inheritdoc/>
+    public bool Equals(BlobHandle other) => Value == other.Value;
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => Value.GetHashCode();
+
+    /// <summary>
+    /// Checks if two <see cref="BlobHandle"/>s are pointing to the same string.
+    /// </summary>
+    /// <param name="left">The first handle.</param>
+    /// <param name="right">The second handle.</param>
+    /// <remarks>
+    /// If <paramref name="left"/> and <paramref name="right"/> come
+    /// from different <see cref="Grammar"/>s the result is undefined.
+    /// </remarks>
+    public static bool operator ==(BlobHandle left, BlobHandle right) => left.Equals(right);
+
+    /// <summary>
+    /// Checks if two <see cref="BlobHandle"/>s are not pointing to the same string.
+    /// </summary>
+    /// <param name="left">The first handle.</param>
+    /// <param name="right">The second handle.</param>
+    /// <remarks>
+    /// If <paramref name="left"/> and <paramref name="right"/> come
+    /// from different <see cref="Grammar"/>s the result is undefined.
+    /// </remarks>
+    public static bool operator !=(BlobHandle left, BlobHandle right) => !(left==right);
+}
---
+++ src/FarkleNeo/Grammars/StringHandle.cs
@@ -0,0 +1,50 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+namespace Farkle.Grammars;
+
+/// <summary>
+/// Represents a reference to a string in a <see cref="Grammar"/>.
+/// </summary>
+public readonly struct StringHandle : IEquatable<StringHandle>
+{
+    internal uint Value { get; }
+
+    internal StringHandle(uint value) => Value = value;
+
+    /// <summary>
+    /// Whether this <see cref="StringHandle"/> points to the empty blob.
+    /// </summary>
+    public bool IsNil => Value == 0;
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is StringHandle handle && Equals(handle);
+
+    /// <inheritdoc/>
+    public bool Equals(StringHandle other) => Value == other.Value;
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => Value.GetHashCode();
+
+    /// <summary>
+    /// Checks if two <see cref="StringHandle"/>s are pointing to the same string.
+    /// </summary>
+    /// <param name="left">The first handle.</param>
+    /// <param name="right">The second handle.</param>
+    /// <remarks>
+    /// If <paramref name="left"/> and <paramref name="right"/> come
+    /// from different <see cref="Grammar"/>s the result is undefined.
+    /// </remarks>
+    public static bool operator ==(StringHandle left, StringHandle right) => left.Equals(right);
+
+    /// <summary>
+    /// Checks if two <see cref="StringHandle"/>s are not pointing to the same string.
+    /// </summary>
+    /// <param name="left">The first handle.</param>
+    /// <param name="right">The second handle.</param>
+    /// <remarks>
+    /// If <paramref name="left"/> and <paramref name="right"/> come
+    /// from different <see cref="Grammar"/>s the result is undefined.
+    /// </remarks>
+    public static bool operator !=(StringHandle left, StringHandle right) => !(left==right);
+}

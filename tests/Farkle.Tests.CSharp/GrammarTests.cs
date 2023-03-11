caa2ddb1354777baa9433355fcc16d30c10d09f9
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-03-11 17:00:28 +0200

    Add a test that loads gramamrs and accesses their properties.

--- tests/Farkle.Tests.CSharp/GrammarTests.cs
+++ tests/Farkle.Tests.CSharp/GrammarTests.cs
@@ -57,4 +57,76 @@ internal class GrammarTests
             return GoldGrammarConverter.Convert(GoldGrammarReader.ReadGrammar(stream));
         }
     }
+
+    [TestCaseSource(typeof(TestUtilities), nameof(TestUtilities.Farkle7Grammars))]
+    public void TestReadGrammar(string grammarFile)
+    {
+        var filePath = TestUtilities.GetResourceFile(grammarFile);
+        var buffer = File.ReadAllBytes(filePath);
+
+        var grammar = Grammar.Create(buffer);
+
+        Assert.Multiple(() =>
+        {
+            foreach (var tokenSymbol in grammar.TokenSymbols)
+            {
+                Assert.That(tokenSymbol.Name.IsEmpty, Is.False);
+                Assert.That(() => tokenSymbol.Attributes, Throws.Nothing);
+            }
+
+            foreach (var nonterminal in grammar.Nonterminals)
+            {
+                Assert.That(nonterminal.Name.IsEmpty, Is.False);
+                Assert.That(() => nonterminal.Attributes, Throws.Nothing);
+            }
+
+            foreach (var group in grammar.Groups)
+            {
+                Assert.That(group.Name.IsEmpty, Is.False);
+                Assert.That(group.Container.HasValue);
+                Assert.That(() => group.Attributes, Throws.Nothing);
+                Assert.That(group.Start.HasValue);
+                Assert.That(group.End.HasValue);
+                foreach (var nesting in group.Nesting)
+                {
+                    Assert.That(nesting.Name.IsEmpty, Is.False);
+                }
+            }
+
+            foreach (var production in grammar.Productions)
+            {
+                Assert.That(production.Head.HasValue);
+                foreach (var member in production.Members)
+                {
+                    Assert.That(member.HasValue);
+                }
+            }
+            if (grammar.DfaOnChar is { } dfa)
+            {
+                int count = 0;
+                foreach (var state in dfa)
+                {
+                    Assert.That(state.StateIndex, Is.EqualTo(count));
+                    Assert.That(() => state.DefaultTransition, Throws.Nothing);
+                    Assert.That(() => { foreach (var edge in state.Edges) { } }, Throws.Nothing);
+                    Assert.That(() => { foreach (var acceptSymbol in state.AcceptSymbols) { } }, Throws.Nothing);
+                    count++;
+                }
+                Assert.That(count, Is.EqualTo(dfa.Count));
+            }
+            if (grammar.LrStateMachine is { } lr)
+            {
+                int count = 0;
+                foreach (var state in lr)
+                {
+                    Assert.That(state.StateIndex, Is.EqualTo(count));
+                    Assert.That(() => { foreach (var action in state.Actions) { } }, Throws.Nothing);
+                    Assert.That(() => { foreach (var action in state.EndOfFileActions) { } }, Throws.Nothing);
+                    Assert.That(() => { foreach (var @goto in state.Gotos) { } }, Throws.Nothing);
+                    count++;
+                }
+                Assert.That(count, Is.EqualTo(lr.Count));
+            }
+        });
+    }
 }

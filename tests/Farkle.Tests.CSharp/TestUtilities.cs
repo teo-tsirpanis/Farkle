f209fdba1c6fb19865781334cda3048606c8d6cd
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-03-11 15:07:36 +0200

    Add an NUnit test case source that looks for Farkle 7 grammar files.

--- tests/Farkle.Tests.CSharp/TestUtilities.cs
+++ tests/Farkle.Tests.CSharp/TestUtilities.cs
@@ -5,5 +5,7 @@ namespace Farkle.Tests.CSharp;
 
 public static class TestUtilities
 {
+    public static IEnumerable<string> Farkle7Grammars => Directory.EnumerateFiles(AppContext.BaseDirectory, "*.grammar.dat");
+
     public static string GetResourceFile(string fileName) => Path.Combine(AppContext.BaseDirectory, fileName);
 }

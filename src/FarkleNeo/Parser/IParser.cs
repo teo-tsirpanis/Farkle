763fbb3ef4a758e720b8472b7d4f4ccc783ef3f8
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-08-02 03:11:25 +0300

    Update the documentation of `IParser`.

--- src/FarkleNeo/Parser/IParser.cs
+++ src/FarkleNeo/Parser/IParser.cs
@@ -4,12 +4,12 @@
 namespace Farkle.Parser;
 
 /// <summary>
-/// Encapsulates the logic of converting characters into meaningful objects.
+/// Encapsulates the logic of converting sequences of characters into meaningful objects.
 /// </summary>
 /// <typeparam name="TChar">The type of characters that are parsed. Usually it is
 /// <see cref="char"/> or <see cref="byte"/> (not supported by Farkle's built-in
 /// parsers).</typeparam>
-/// <typeparam name="T">The type of result the parser produces in case of success.</typeparam>
+/// <typeparam name="T">The type of objects the parser produces in case of success.</typeparam>
 /// <remarks>
 /// <para>
 /// This is the base interface for Farkle's parser API and encapsulates the lexical,
@@ -52,6 +52,14 @@ public interface IParser<TChar,T> : IServiceProvider
     /// property is set to <see langword="true"/> should set a result to <paramref name="completionState"/>.
     /// Custom parsers can support deviating from this behavior, but must do it in an opt-in fashion.
     /// </para>
+    /// <para>
+    /// While using this method to parse a single contiguous buffer is simple, directly using it
+    /// to parse streaming input is non-trivial and requires manually managing the character buffer.
+    /// Parsing streaming input is best done by using <see cref="ParserExtensions"/> or
+    /// a <see cref="ParserStateContext{TChar, T}"/>.
+    /// </para>
     /// </remarks>
+    /// <seealso cref="ParserExtensions"/>
+    /// <seealso cref="ParserStateContext"/>
     void Run(ref ParserInputReader<TChar> inputReader, ref ParserCompletionState<T> completionState);
 }

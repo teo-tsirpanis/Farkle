f24826ec86c0b66674d762aa65feafa11875aa9a
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-02-02 03:49:20 +0200

    Emit diagnostics when the DFA state limit is reached.

--- src/FarkleNeo/Builder/BuilderLoggerExtensions.cs
+++ src/FarkleNeo/Builder/BuilderLoggerExtensions.cs
@@ -7,6 +7,12 @@ namespace Farkle.Builder;
 
 internal static class BuilderLoggerExtensions
 {
+    private static void Error(in this BuilderLogger logger, string code, object message) =>
+        logger.Log(DiagnosticSeverity.Error, message, code);
+
+    public static void DfaStateLimitExceeded(in this BuilderLogger logger, int maxStates) =>
+        logger.Error("FARKLE0001", LocalizedDiagnostic.Create(nameof(Resources.Builder_DfaStateLimitExceeded), maxStates));
+
     public static void Debug(in this BuilderLogger logger, string message) => logger.Log(DiagnosticSeverity.Debug, message);
 
     public static void Verbose(in this BuilderLogger logger, string message) => logger.Log(DiagnosticSeverity.Verbose, message);

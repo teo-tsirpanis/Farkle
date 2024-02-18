6c4016aa72ea7e2109a07f42fd4584a14877e0ba
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-02-18 02:58:10 +0200

    Add the base types of the builder's object model.

---
+++ src/FarkleNeo/Builder/Nonterminal.cs
@@ -0,0 +1,189 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+using System.Collections.Immutable;
+
+namespace Farkle.Builder;
+
+/// <summary>
+/// Provides factory methods to define nonterminals.
+/// </summary>
+public static class Nonterminal
+{
+    /// <summary>
+    /// Creates a nonterminal that produces a value, whose productions must be assigned at a later time.
+    /// </summary>
+    /// <typeparam name="T">The type of values the nonterminal will produce.</typeparam>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <seealso cref="Nonterminal{T}.SetProductions(IProduction{T}[])"/>
+    /// <seealso cref="Nonterminal{T}.SetProductions(ImmutableArray{IProduction{T}})"/>
+    public static Nonterminal<T> Create<T>(string name)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        return new(name);
+    }
+
+    /// <summary>
+    /// Creates a nonterminal that produces a value.
+    /// </summary>
+    /// <typeparam name="T">The type of values the nonterminal will produce.</typeparam>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <param name="productions">The nonterminal's productions.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    public static IGrammarSymbol<T> Create<T>(string name, params IProduction<T>[] productions)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        ArgumentNullExceptionCompat.ThrowIfNull(productions);
+        return Create(name, productions.ToImmutableArray());
+    }
+
+    /// <summary>
+    /// Creates a nonterminal that produces a value.
+    /// </summary>
+    /// <typeparam name="T">The type of values the nonterminal will produce.</typeparam>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <param name="productions">The nonterminal's productions.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    public static IGrammarSymbol<T> Create<T>(string name, ImmutableArray<IProduction<T>> productions)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        if (productions.IsDefault)
+        {
+            ThrowHelpers.ThrowArgumentNullException(nameof(productions));
+        }
+        if (productions.IsEmpty)
+        {
+            ThrowHelpers.ThrowArgumentException("Cannot create a nonterminal with no productions", nameof(productions));
+        }
+        return new Nonterminal<T>(name, productions);
+    }
+
+    /// <summary>
+    /// Creates a nonterminal that does not produce a value and whose productions
+    /// must be assigned at a later time.
+    /// </summary>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <seealso cref="Untyped.Nonterminal.SetProductions(ProductionBuilder[])"/>
+    /// <seealso cref="Untyped.Nonterminal.SetProductions(ImmutableArray{ProductionBuilder})"/>
+    public static Untyped.Nonterminal CreateUntyped(string name)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        return new(name);
+    }
+
+    /// <summary>
+    /// Creates a nonterminal that does not produce a value.
+    /// </summary>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <param name="productions">The nonterminal's productions, represented as <see cref="ProductionBuilder"/>
+    /// objects that have not been <c>Extend</c>ed or <c>Finish</c>ed.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    public static IGrammarSymbol CreateUntyped(string name, params ProductionBuilder[] productions)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        ArgumentNullExceptionCompat.ThrowIfNull(productions);
+        return CreateUntyped(name, productions.ToImmutableArray());
+    }
+
+    /// <summary>
+    /// Creates a nonterminal that does not produce a value.
+    /// </summary>
+    /// <param name="name">The nonterminal's name.</param>
+    /// <param name="productions">The nonterminal's productions, represented as <see cref="ProductionBuilder"/>
+    /// objects that have not been <c>Extend</c>ed or <c>Finish</c>ed.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    public static IGrammarSymbol CreateUntyped(string name, ImmutableArray<ProductionBuilder> productions)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(name);
+        if (productions.IsDefault)
+        {
+            ThrowHelpers.ThrowArgumentNullException(nameof(productions));
+        }
+        if (productions.IsEmpty)
+        {
+            ThrowHelpers.ThrowArgumentException("Cannot create a nonterminal with no productions", nameof(productions));
+        }
+        return new Untyped.Nonterminal(name, productions);
+    }
+}
+
+/// <summary>
+/// Represents a nonterminal symbol in a grammar to be built that produces a value,
+/// and allows setting its productions after its creation.
+/// </summary>
+/// <typeparam name="T">The type of values the nonterminal will produce.</typeparam>
+/// <remarks>
+/// In Farkle, builder objects are usually immutable. This exception exists to support
+/// defining recursive nonterminals.
+/// </remarks>
+// Keep this synchronized with the untyped Nonterminal class at Untyped.cs.
+public sealed class Nonterminal<T> : INonterminal, IGrammarSymbol<T>
+{
+    /// <summary>
+    /// The nonterminal's productions.
+    /// </summary>
+    /// <remarks>
+    /// Valid states:
+    /// <list type="bullet">
+    /// <item><description><see langword="default"/>: the productions have not been set yet</description></item>
+    /// <item><description>empty the nonterminal has been used in building a grammar, but no productions had been set yet</description></item>
+    /// <item><description>non-empty: the productions have been set</description></item>
+    /// </list>
+    /// </remarks>
+    private ImmutableArray<IProduction<T>> _productions;
+
+    /// <inheritdoc/>
+    public string Name { get; }
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+
+    internal Nonterminal(string name, ImmutableArray<IProduction<T>> productions = default)
+    {
+        Name = name;
+        _productions = productions;
+    }
+
+    /// <summary>
+    /// Sets the productions of this nonterminal.
+    /// </summary>
+    /// <param name="productions">An array with the productions to set.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    /// <exception cref="InvalidOperationException">The productions have already been successfully set.</exception>
+    /// <remarks>This function and its overloads must be called exactly once, and before the
+    /// nonterminal is used in building a grammar.</remarks>
+    public void SetProductions(params IProduction<T>[] productions)
+    {
+        ArgumentNullExceptionCompat.ThrowIfNull(productions);
+        SetProductions(productions.ToImmutableArray());
+    }
+
+    /// <summary>
+    /// Sets the productions of this nonterminal.
+    /// </summary>
+    /// <param name="productions">An immutable array with the productions to set.</param>
+    /// <exception cref="ArgumentException"><paramref name="productions"/> is empty.</exception>
+    /// <exception cref="InvalidOperationException">The productions have already been successfully set.</exception>
+    /// <remarks>This function and its overloads must be called exactly once, and before the
+    /// nonterminal is used in building a grammar.</remarks>
+    public void SetProductions(ImmutableArray<IProduction<T>> productions)
+    {
+        if (productions.IsDefault)
+        {
+            ThrowHelpers.ThrowArgumentNullException(nameof(productions));
+        }
+        if (productions.IsEmpty)
+        {
+            ThrowHelpers.ThrowArgumentException("Productions cannot be empty", nameof(productions));
+        }
+        if (!ImmutableInterlocked.InterlockedCompareExchange(ref _productions, productions, default).IsDefault)
+        {
+            throw new InvalidOperationException("Cannot set productions of a nonterminal more than once.");
+        }
+    }
+
+    ImmutableArray<IProduction> INonterminal.FreezeAndGetProductions()
+    {
+        ImmutableInterlocked.InterlockedCompareExchange(ref _productions, [], default);
+        return _productions.CastArray<IProduction>();
+    }
+}
---
+++ src/FarkleNeo/Builder/ObjectModel.cs
@@ -0,0 +1,221 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+// This file contains the base definitions of the builder's object model.
+
+using System.Collections.Immutable;
+
+namespace Farkle.Builder;
+
+/// <summary>
+/// Represents a grammar to be built, containing syntax and semantic rules.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This is the base interface of the object model of Farkle's builder
+/// and cannot be implemented by user code.
+/// </para>
+/// <para>
+/// The only operations allowed on this interface are building it and
+/// setting global options.
+/// </para>
+/// </remarks>
+public interface IGrammarBuilder
+{
+    internal ISymbolBase Symbol { get; }
+}
+
+/// <summary>
+/// Augments <see cref="IGrammarBuilder"/> to represent a grammar to be built
+/// with a known type of values it produces.
+/// </summary>
+/// <typeparam name="T">The type of values the grammar will produce.</typeparam>
+/// <remarks>
+/// This interface cannot be implemented by user code.
+/// </remarks>
+public interface IGrammarBuilder<out T> : IGrammarBuilder;
+
+/// <summary>
+/// Represents a symbol (terminal or nonterminal) in a grammar to be built that can be
+/// composed to form more complex symbols.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This interface inherits from <see cref="IGrammarBuilder"/> and also represents a
+/// grmmar to be built with this symbol as its start symbol. It cannot be implemented
+/// by user code.
+/// </para>
+/// <para>
+/// This interface is the closest replacement of Farkle 6's <c>DesigntimeFarkle</c>
+/// interface. The functionality of that interface has been split to this interface
+/// and <see cref="IGrammarBuilder"/>, to codify in the type system which configuration
+/// operations apply to individual symbols and which apply to the whole grammar.
+/// </para>
+/// </remarks>
+/// <seealso cref="GrammarSymbolExtensions"/>
+public interface IGrammarSymbol : IGrammarBuilder
+{
+    /// <summary>
+    /// The symbol's name.
+    /// </summary>
+    /// <remarks>
+    /// This value is used only for diagnostics purposes and does not affect the
+    /// grammar's behavior when parsing. A grammar may contain multiple symbols
+    /// with the same name.
+    /// </remarks>
+    string Name { get; }
+}
+
+/// <summary>
+/// Combines <see cref="IGrammarSymbol"/> and <see cref="IGrammarBuilder{T}"/> to represent
+/// a symbol in a grammar to be built with a known type of values it produces.
+/// </summary>
+/// <typeparam name="T">The type of values the symbol will produce.</typeparam>
+/// <remarks>
+/// <para>
+/// This interface cannot be implemented by user code.
+/// </para>
+/// <para>
+/// This interface is the closest replacement of Farkle 6's <c>DesigntimeFarkle&lt;TResult&gt;</c>
+/// interface. The functionality of that interface has been split to this interface
+/// and <see cref="IGrammarBuilder"/>, to codify in the type system which configuration
+/// operations apply to individual symbols and which apply to the whole grammar.
+/// </para>
+/// </remarks>
+/// <seealso cref="GrammarSymbolExtensions"/>
+/// <seealso cref="IGrammarBuilder"/>
+public interface IGrammarSymbol<out T> : IGrammarBuilder<T>, IGrammarSymbol;
+
+/// <summary>
+/// Marker interface for the types of concrete symbols in a grammar to be built,
+/// as opposed to wrapper classes that change configuration options.
+/// </summary>
+internal interface ISymbolBase : IGrammarSymbol;
+
+// Terminal also contains public factory methods so it is public and partial.
+// No public API must return objects of type Terminal, only IGrammarSymbol(<T>).
+public partial class Terminal : ISymbolBase
+{
+    internal string Name { get; }
+
+    internal Regex Regex { get; }
+
+    internal Transformer<char, object?> Transformer { get; }
+
+    internal Terminal(string name, Regex regex, Transformer<char, object?> transformer)
+    {
+        Name = name;
+        Regex = regex;
+        Transformer = transformer;
+    }
+
+    string IGrammarSymbol.Name => Name;
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+}
+
+internal sealed class VirtualTerminal(string name) : ISymbolBase
+{
+    public string Name { get; } = name;
+
+    public string Value => Name;
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+}
+
+internal sealed class Literal(string value) : ISymbolBase
+{
+    public string Name => Value;
+
+    public string Value { get; } = value;
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+}
+
+internal sealed class NewLine : ISymbolBase
+{
+    public static NewLine Instance { get; } = new();
+
+    public string Name => nameof(NewLine);
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+
+    private NewLine() { }
+}
+
+// Group is a similar case to Terminal, so it is public and partial.
+// No public API must return objects of type Group, only IGrammarSymbol(<T>).
+public abstract partial class Group : ISymbolBase
+{
+    internal string Name { get; }
+
+    internal string GroupStart { get; }
+
+    internal Transformer<char, object?> Transformer { get; }
+
+    private protected Group(string name, string groupStart, Transformer<char, object?> transformer)
+    {
+        Name = name;
+        GroupStart = groupStart;
+        Transformer = transformer;
+    }
+
+    string IGrammarSymbol.Name => Name;
+
+    ISymbolBase IGrammarBuilder.Symbol => this;
+}
+
+internal class LineGroup(string name, string groupStart, Transformer<char, object?> transformer) : Group(name, groupStart, transformer);
+
+internal class BlockGroup(string name, string groupStart, string groupEnd, Transformer<char, object?> transformer) : Group(name, groupStart, transformer)
+{
+    public string GroupEnd { get; } = groupEnd;
+}
+
+internal interface IProduction {
+    ImmutableArray<IGrammarSymbol> Members { get; }
+
+    Fuser<object?> Fuser { get; }
+
+    object? PrecedenceToken { get; }
+}
+
+// This is an interface because both Nonterminal and Nonterminal<T>
+// must be public, and the former cannot inherit from the latter.
+internal interface INonterminal : ISymbolBase
+{
+    ImmutableArray<IProduction> FreezeAndGetProductions();
+}
+
+internal sealed class Terminal<T>(string name, Regex regex, Transformer<char, object?> transformer) : Terminal(name, regex, transformer), IGrammarSymbol<T>;
+
+internal sealed class LineGroup<T>(string name, string groupStart, Transformer<char, object?> transformer) : LineGroup(name, groupStart, transformer), IGrammarSymbol<T>;
+
+internal sealed class BlockGroup<T>(string name, string groupStart, string groupEnd, Transformer<char, object?> transformer) : BlockGroup(name, groupStart, groupEnd, transformer), IGrammarSymbol<T>;
+
+/// <summary>
+/// Represents a production in a grammar to be built that produces a value.
+/// </summary>
+/// <typeparam name="T">The type of values the production will produce.</typeparam>
+/// <remarks>
+/// This interface cannot be implemented by user code.
+/// </remarks>
+/// <seealso cref="Nonterminal.Create{T}(string, IProduction{T}[])"/>
+/// <seealso cref="Nonterminal{T}.SetProductions(IProduction{T}[])"/>
+/// <seealso cref="Nonterminal{T}.SetProductions(ImmutableArray{IProduction{T}})"/>
+public interface IProduction<out T> {
+    // We cannot inherit IProduction because we want the generic interface to be public.
+    // Instead, we expose the IProduction through this property.
+    internal IProduction Production { get; }
+}
+
+internal class Production<T>(ImmutableArray<IGrammarSymbol> symbols, Fuser<object?> fuser, object? precedenceToken) : IProduction, IProduction<T>
+{
+    public ImmutableArray<IGrammarSymbol> Members { get; } = symbols;
+
+    public Fuser<object?> Fuser { get; } = fuser;
+
+    public object? PrecedenceToken { get; } = precedenceToken;
+
+    IProduction IProduction<T>.Production => this;
+}

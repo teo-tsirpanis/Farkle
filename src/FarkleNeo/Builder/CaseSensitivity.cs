128dde2eb0b3fa3c94019b95aeb61281915a9134
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-02-22 02:54:19 +0200

    Add configuration options for grammars and symbols.

---
+++ src/FarkleNeo/Builder/CaseSensitivity.cs
@@ -0,0 +1,33 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+namespace Farkle.Builder;
+
+/// <summary>
+/// Specifies the case sensitivity of a grammar to be built.
+/// </summary>
+/// <seealso cref="GrammarBuilderConfigurationExtensions.CaseSensitive(IGrammarBuilder, CaseSensitivity)"/>
+public enum CaseSensitivity
+{
+    /// <summary>
+    /// The entire grammar is case sensitive.
+    /// </summary>
+    /// <remarks>
+    /// This is the default value since Farkle 7.
+    /// </remarks>
+    CaseSensitive,
+    /// <summary>
+    /// Only the literals of the grammar are case insensitive and the other terminals are case
+    /// sensitive.
+    /// </summary>
+    /// <seealso cref="Terminal.Literal"/>
+    LiteralsCaseInsensitive,
+    /// <summary>
+    /// The entire grammar is case insensitive.
+    /// </summary>
+    /// <remarks>
+    /// This was the default value in versions of Farkle prior to 7,
+    /// but was changed to <see cref="CaseSensitive"/> for performance reasons.
+    /// </remarks>
+    CaseInsensitive
+}

a0111880d43ae9e02f9af984157eb70b54461fb4
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-03-05 03:49:54 +0200

    Obsolete `AsIs()`.

---
+++ src/FarkleNeo/Obsoletions.cs
@@ -0,0 +1,12 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+namespace Farkle;
+
+internal static class Obsoletions
+{
+    public const string SharedUrlFormat = "https://github.com/teo-tsirpanis/Farkle/blob/mainstream/docs/diagnostics/{0}.md";
+
+    public const string AsIsApiCode = "FARKLE1001";
+    public const string AsIsApiMessage = "Use AsProduction() instead.";
+}

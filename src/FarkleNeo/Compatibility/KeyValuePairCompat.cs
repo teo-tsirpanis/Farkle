6e23157fbbbb4946497cc3ba1959f7e11848ae78
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-05-11 13:08:59 +0300

    Fix bugs discovered by the grammar equivalence tests.

---
+++ src/FarkleNeo/Compatibility/KeyValuePairCompat.cs
@@ -0,0 +1,16 @@
+// Copyright © Theodore Tsirpanis and Contributors.
+// SPDX-License-Identifier: MIT
+
+#if !(NETCOREAPP || NETSTANDARD2_1_OR_GREATER)
+namespace System
+{
+    internal static class KeyValuePairCompat
+    {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value)
+        {
+            key = kvp.Key;
+            value = kvp.Value;
+        }
+    }
+}
+#endif

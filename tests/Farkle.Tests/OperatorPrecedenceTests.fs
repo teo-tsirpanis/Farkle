d7fcf00e3407a4c767d1a94ba2935b0bad82ae6d
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-05-26 14:21:59 +0300

    Fix compile errors in the operator precedence tests.

--- tests/Farkle.Tests/OperatorPrecedenceTests.fs
+++ tests/Farkle.Tests/OperatorPrecedenceTests.fs
@@ -7,12 +7,12 @@ module Farkle.Tests.OperatorPrecedenceTests
 
 open Expecto
 open Farkle
-open Farkle.Samples.FSharp
 open Farkle.Builder
 open Farkle.Builder.OperatorPrecedence
 
 [<Tests>]
 let tests = testList "Operator precedence tests" [
+#if false // TODO-FARKLE7: Reevaluate when the samples are ported to Farkle 7.
     test "The calculator respects operator precedence and associativity" {
         let runtime = SimpleMaths.int
         let testData = [
@@ -23,47 +23,19 @@ let tests = testList "Operator precedence tests" [
         for expr, result in testData do
             Expect.equal (runtime.Parse expr) (Ok result) (sprintf "Parsing '%s' failed" expr)
     }
-
-    test "Precedence among symbols from different groups cannot be compared" {
-        let grammar =
-            let expr = nonterminalU "Expr"
-
-            let opScope1 = OperatorScope(LeftAssociative("+"))
-            let opScope2 = OperatorScope(LeftAssociative("-"))
-
-            let number =
-                Terminals.int "Number"
-                |> DesigntimeFarkle.withOperatorScope opScope1
-
-            expr.SetProductions(
-                !% expr .>> "+" .>> expr,
-                !% expr .>> "-" .>> expr,
-                !% number
-            )
-
-            expr
-            |> DesigntimeFarkle.withOperatorScope opScope2
-            |> DesigntimeFarkleBuild.createGrammarDefinition
-            |> DesigntimeFarkleBuild.buildGrammarOnly
-
-        Expect.wantError grammar "Building had succeeded"
-        |> Flip.Expect.all "Not all conflicts could not be resolved because of different operator scopes" (function
-        | BuildError.LALRConflictReport _
-        | BuildError.LALRConflict {Reason = DifferentOperatorScope} -> true
-        | _ -> false)
-    }
+#endif
 
     test "Reduce-Reduce conflicts are resolved only on demand" {
         let mkGrammar (resolveRRConflicts: bool) =
             let prec1 = obj()
             let prec2 = obj()
 
-            let x1 = "X1" |||= [empty |> prec prec1]
-            let x2 = "X2" |||= [empty |> prec prec2]
+            let x1 = "X1" ||= [empty |> prec prec1 =% 1]
+            let x2 = "X2" ||= [empty |> prec prec2 =% 2]
 
-            let expr = "Expr" |||= [
-                !& "x" .>> x1
-                !& "x" .>> x2
+            let expr = "Expr" ||= [
+                !& "x" .>>. x1 |> asProduction
+                !& "x" .>>. x2 |> asProduction
             ]
 
             let opScope =
@@ -73,33 +45,22 @@ let tests = testList "Operator precedence tests" [
                 )
 
             expr
-            |> DesigntimeFarkle.autoWhitespace false
-            |> DesigntimeFarkle.withOperatorScope opScope
+            |> _.AutoWhitespace(false)
+            |> _.WithOperatorScope(opScope)
 
         let grammarNotResolved =
             mkGrammar false
-            |> DesigntimeFarkleBuild.createGrammarDefinition
-            |> DesigntimeFarkleBuild.buildGrammarOnly
-        Expect.wantError grammarNotResolved "Building had succeeded"
-        |> Flip.Expect.all "Not all conflicts could not be resolved because of the operator scope's inability" (function
-        | BuildError.LALRConflictReport _
-        | BuildError.LALRConflict {Reason = CannotResolveReduceReduce; Type = ReduceReduce _} -> true
-        | _ -> false)
+            |> GrammarBuilder.build
+        Expect.isTrue grammarNotResolved.IsFailing "Building should have failed"
+        let lrStateMachine = grammarNotResolved.GetGrammar().LrStateMachine
+        Expect.isNotNull lrStateMachine "The LR state machine was not built"
+        Expect.isTrue lrStateMachine.HasConflicts "The LR state machine does not have conflicts"
 
         let runtimeResolved =
             mkGrammar true
-            |> RuntimeFarkle.buildUntyped
-            |> RuntimeFarkle.changePostProcessor PostProcessors.ast
-        Expect.isTrue runtimeResolved.IsBuildSuccessful "Building failed"
-
-        let expectedAST =
-            let grammar = runtimeResolved.GetGrammar()
-            let nont idx xs = AST.Nonterminal(grammar.Productions.[idx], xs)
-            nont 1 [
-                AST.Content(grammar.GetTerminalByName "x", Position.Initial, "x")
-                nont 3 []
-            ]
-        Expect.equal (runtimeResolved.Parse "x") (Ok expectedAST) "The parsed AST is different than the expected"
+            |> GrammarBuilder.build
+        Expect.isFalse runtimeResolved.IsFailing "Building failed"
+        Expect.equal (runtimeResolved.Parse "x") (ParserResult.CreateSuccess 2) "The resolved reduction is different than the expected"
     }
 
     test "Non-associative operators work" {
@@ -109,17 +70,17 @@ let tests = testList "Operator precedence tests" [
             expr.SetProductions(
                 !@ expr .>> "+" .>>. expr => (+),
                 !@ expr .>> "*" .>>. expr => ( * ),
-                !@ number |> asIs
+                !@ number |> asProduction
             )
 
-            // Both the string literal and the designtime Farkle literal should be recognized as equals.
+            // Both the string literal and the designtime Farkle literal should be equivalent.
             let opScope = OperatorScope(LeftAssociative("+"), NonAssociative(literal "*"))
 
             expr
-            |> DesigntimeFarkle.withOperatorScope opScope
-            |> RuntimeFarkle.build
+            |> _.WithOperatorScope(opScope)
+            |> GrammarBuilder.build
 
-        Expect.equal (runtime.Parse "3+4+5") (Ok 12) "Parsing failed"
-        Expect.isError (runtime.Parse "3*4*5") "Parsing did not fail"
+        Expect.equal (runtime.Parse "3+4+5") (ParserResult.CreateSuccess 12) "Parsing failed"
+        expectIsParseFailure (runtime.Parse "3*4*5") "Parsing did not fail"
     }
 ]

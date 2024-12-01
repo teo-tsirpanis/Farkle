79d028c97287eb35df500e9c1e773fbc3b0ec557
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-12-01 19:45:08 +0200

    Port the HTML templates to FarkleNeo.

--- src/Farkle.Tools.Shared/Templating/GrammarFunctions.fs
+++ src/Farkle.Tools.Shared/Templating/GrammarFunctions.fs
@@ -29,10 +29,32 @@ type GrammarFunctions(g: GrammarTemplateInput) =
     let grammarSO =
         let so = ScriptObject()
         so.Import(grammarObj, filter = grammarMemberFilter)
-        so.SetValue("productions_grouped", grammarObj.Productions.ToLookup(_.Head), true)
+        so.Import("get_object_from_handle", Func<obj,_>(function
+            | :? EntityHandle as h when h.IsTokenSymbol -> TokenSymbolHandle.op_Explicit h |> grammarObj.GetTokenSymbol |> box
+            | :? EntityHandle as h when h.IsNonterminal -> NonterminalHandle.op_Explicit h |> grammarObj.GetNonterminal |> box
+            | :? EntityHandle as h when h.IsProduction -> ProductionHandle.op_Explicit h |> grammarObj.GetProduction |> box
+            | :? TokenSymbolHandle as h -> grammarObj.GetTokenSymbol h |> box
+            | :? NonterminalHandle as h -> grammarObj.GetNonterminal h |> box
+            | :? ProductionHandle as h -> grammarObj.GetProduction h |> box
+            | x -> failwith $"invlid object '{x.GetType()}'; must be a grammar object handle or EntityHandle"))
+        so.Import("is_terminal", Func<_,_>(fun x -> grammarObj.IsTerminal x))
         so
 
     member _.Grammar = grammarSO
+
+    static member is_terminal x = (x &&& TokenSymbolAttributes.Terminal) <> TokenSymbolAttributes.None
+    static member is_group_start x = (x &&& TokenSymbolAttributes.GroupStart) <> TokenSymbolAttributes.None
+    static member is_hidden x = (x &&& TokenSymbolAttributes.Hidden) <> TokenSymbolAttributes.None
+    static member is_noise x = (x &&& TokenSymbolAttributes.Noise) <> TokenSymbolAttributes.None
+    static member is_generated (x: obj) =
+        match x with
+        | :? TokenSymbolAttributes as x -> (x &&& TokenSymbolAttributes.Generated) <> TokenSymbolAttributes.None
+        | :? NonterminalAttributes as x -> (x &&& NonterminalAttributes.Generated) <> NonterminalAttributes.None
+        | _ -> false
+
+    static member is_ends_on_end_of_input x = (x &&& GroupAttributes.EndsOnEndOfInput) <> GroupAttributes.None
+    static member is_advance_by_character x = (x &&& GroupAttributes.AdvanceByCharacter) <> GroupAttributes.None
+    static member is_keep_end_token x = (x &&& GroupAttributes.KeepEndToken) <> GroupAttributes.None
 
     static member group_dfa_edges (state: StateMachines.DfaState<char>) =
         state.Edges.ToLookup(_.Target).OrderBy(_.Key)

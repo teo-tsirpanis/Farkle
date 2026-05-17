353c7cbedf651114c948f4a2d187cffab5c6a3ea
Theodore Tsirpanis <teo@tsirpanis.gr>
2026-05-17 18:43:10 +0300

    Restore branchless reads for everything in the grammar.

--- src/Farkle/Grammars/GrammarTables.cs
+++ src/Farkle/Grammars/GrammarTables.cs
@@ -96,13 +96,13 @@ internal readonly struct GrammarTables
     }
 
     private static uint ReadTableIndex(ReadOnlySpan<byte> grammarFile, int index, int rowCount) =>
-        grammarFile.ReadUIntVariableSize(index, GetCompressedIndexSize(rowCount));
+        grammarFile.ReadUIntVariableSizeBranchless(index, GetCompressedIndexSize(rowCount));
 
     private StringHandle ReadStringHandle(ReadOnlySpan<byte> grammarFile, int index) =>
-        new(grammarFile.ReadUIntVariableSize(index, StringHeapIndexSize));
+        new(grammarFile.ReadUIntVariableSizeBranchless(index, StringHeapIndexSize));
 
     private BlobHandle ReadBlobHandle(ReadOnlySpan<byte> grammarFile, int index) =>
-        new(grammarFile.ReadUIntVariableSize(index, BlobHeapIndexSize));
+        new(grammarFile.ReadUIntVariableSizeBranchless(index, BlobHeapIndexSize));
 
     private TokenSymbolHandle ReadTokenSymbolHandle(ReadOnlySpan<byte> grammarFile, int index) =>
         new(ReadTableIndex(grammarFile, index, TokenSymbolRowCount));
